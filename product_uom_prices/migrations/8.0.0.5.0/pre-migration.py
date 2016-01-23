628a59bc39c39654eed197d5b61de91df61677b3
Juan Jose Scarafia <jjs@adhoc.com.ar>
2016-01-23 12:59:12 -0300

    FIX uom migrations

--- product_uom_prices/migrations/8.0.0.5.0/pre-migration.py
+++ product_uom_prices/migrations/8.0.0.5.0/pre-migration.py
@@ -1,36 +1,34 @@
 # -*- encoding: utf-8 -*-
-from openerp import SUPERUSER_ID
-from openerp.modules.registry import RegistryManager
+
+import logging
+
+logger = logging.getLogger('upgrade')
 
 
-def set_value(cr, model, table, field, value, condition):
-    print 'Set value %s on field %s on table %s' % (
-        value, field, table)
-    cr.execute('SELECT id '
-               'FROM %(table)s '
-               '%(condition)s' % {
-                   'table': table,
-                   'condition': condition,
-               })
-    for row in cr.fetchall():
-        model.write(cr, SUPERUSER_ID, row[0], {field: value})
+def get_legacy_name(original_name, version):
+    return 'legacy_%s_%s' % (version.replace('.', '_'), original_name)
+
+
+def rename_columns(cr, column_spec, version):
+    for table, renames in column_spec.iteritems():
+        for old, new in renames:
+            if new is None:
+                new = get_legacy_name(old, version)
+            logger.info("table %s, column %s: renaming to %s",
+                        table, old, new)
+            cr.execute('ALTER TABLE "%s" RENAME "%s" TO "%s"'
+                       % (table, old, new,))
+            cr.execute('DROP INDEX IF EXISTS "%s_%s_index"'
+                       % (table, old))
+
+column_renames = {
+    'product_template': [
+        ('use_uom_prices', None)
+    ]
+}
 
 
 def migrate(cr, version):
-    print 'Migrating product_uom_prices'
     if not version:
         return
-    registry = RegistryManager.get(cr.dbname)
-    model = 'product.template'
-    table = 'product_template'
-    field = "list_price_type"
-    value = "by_uom"
-    condition = "WHERE use_uom_prices"
-    set_value(
-        cr,
-        registry[model],
-        table,
-        field,
-        value,
-        condition,
-    )
+    rename_columns(cr, column_renames, version)
