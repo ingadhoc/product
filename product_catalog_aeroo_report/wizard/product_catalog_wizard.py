7e57ef0d2fde7b68cf227342e092999334e4f1ee
Juan Jose Scarafia <jjs@adhoc.com.ar>
2016-01-27 15:32:43 -0300

    ADD product price taxes included

--- product_catalog_aeroo_report/wizard/product_catalog_wizard.py
+++ product_catalog_aeroo_report/wizard/product_catalog_wizard.py
@@ -11,17 +11,25 @@ class product_catalog(osv.osv_memory):
     _description = 'Wizard to generate the Product Catalog Report with Aeroo'
 
     _columns = {
-        'product_catalog_report_id': fields.many2one('product.product_catalog_report', 'Product Catalog', required=True),
+        'product_catalog_report_id': fields.many2one(
+            'product.product_catalog_report',
+            'Product Catalog',
+            required=True),
+        'taxes_included': fields.boolean('Taxes Included'),
     }
 
     _defaults = {
     }
 
     def generate_report(self, cr, uid, ids, context=None):
+        if not context:
+            context = {}
         wizard = self.browse(cr, uid, ids)[0]
 
         catalog = wizard.product_catalog_report_id
         if not catalog:
             return {'type': 'ir.actions.act_window_close'}
-
-        return self.pool.get('product.product_catalog_report').generate_report(cr, uid, [catalog.id], context)
+        context['taxes_included'] = wizard.taxes_included
+        return self.pool.get(
+            'product.product_catalog_report').generate_report(
+                cr, uid, [catalog.id], context)
