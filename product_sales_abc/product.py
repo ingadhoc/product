aac16d6dd1d77520c10e6cedb8b0752e2d9e58ca
Nicolas Mac Rouillon <nmr@adhoc.com.ar>
2016-05-12 13:45:09 -0300

    ADD product_sales_abc

---
+++ product_sales_abc/product.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+# For copyright and license notices, see __openerp__.py file in module root
+# directory
+##############################################################################
+from openerp import fields, models, api
+
+
+class product_template(models.Model):
+
+    """"""
+
+    _inherit = 'product.template'
+
+    abc_sales_quantity = fields.Char('ABC Sales quantity')
+    abc_sales_amount = fields.Char('ABC Sales amount')
+    abc_sales_combined = fields.Char(
+        compute='_get_abc_total')
+
+    @api.one
+    def _get_abc_total(self):
+        if self.abc_sales_quantity and self.abc_sales_amount:
+            self.abc_sales_combined = self.abc_sales_quantity + self.abc_sales_amount
+        elif self.abc_sales_quantity and not self.abc_sales_amount:
+            self.abc_sales_combined = self.abc_sales_quantity
+        elif self.abc_sales_amount and not self.abc_sales_quantity:
+            self.abc_sales_combined = self.abc_sales_amount
