e4d4e6e45fc7c37a62a6e84dc4feb99d9cec6859
Mohamed M. Hagag <mohamedhagag@users.noreply.github.com>
2016-06-28 03:32:18 +0300

    Add files via upload

---
+++ product_pack_pos/__openerp__.py
@@ -0,0 +1,19 @@
+{
+    'name': 'Product Pack POS for IngAdhoc',
+    'summary': 'Product packs on POS',
+    'description': """
+This module is extension for INGADHOC's module product_pack that will
+Process product_pack pickings from POS sales.
+     """,
+    'version': '8.0.0.1',
+    'category': 'Point of Sale',
+    'author': 'DVIT.me',
+    'website': 'http://dvit.me',
+    'license': 'AGPL-3',
+    'depends': ['product_pack', 'point_of_sale'],
+    'data': [],
+    'demo': [],
+    'installable': True,
+    'auto_install': True,
+    'application': False,
+}
