3baf2b6cf02dc67e4e63d48062f0bc24ac4f8c53
susport <richard.pompa84@gmail.com>
2016-07-30 17:43:17 -0300

    9.0-mig-product_pricelist
    Corrigiendo travis

--- product_pricelist/models/product.py
+++ product_pricelist/models/product.py
@@ -3,8 +3,7 @@
 # For copyright and license notices, see __openerp__.py file in module root
 # directory
 ##############################################################################
-from openerp import models, fields, api, _
-from openerp.exceptions import Warning
+from openerp import models, fields, api
 import logging
 _logger = logging.getLogger(__name__)
 
