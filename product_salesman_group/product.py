d331a853bdfe238e29fe4f85dea625cf00978bdc
susport <richard.pompa84@gmail.com>
2016-07-30 22:45:01 -0300

    9.0-mig-product_salesman_group
    Corrigiendo errores

--- product_salesman_group/product.py
+++ product_salesman_group/product.py
@@ -3,7 +3,7 @@
 # For copyright and license notices, see __openerp__.py file in module root
 # directory
 ##############################################################################
-from openerp import models, api, fields
+from openerp import models, fields
 
 
 class product_template(models.Model):
@@ -13,14 +13,5 @@ class product_template(models.Model):
     salesman_group_ids = fields.Many2many(
         'sale.salesman.group', 'prod_template_salesgroup_rel',
         'template_id', 'section_id', string='Salesman Group', )
-
-class product_product(models.Model):
-
-    _inherit = 'product.product'
-
-    salesman_group_ids = fields.Many2many(
-        'sale.salesman.group', 'prod_template_salesgroup_rel',
-        'template_id', 'section_id', string='Salesman Group', ),
-
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
