8158aa3468bf69c701e92b7b4d4fe9afeb884ee0
susport <richard.pompa84@gmail.com>
2016-07-30 22:45:01 -0300

    9.0-mig-product_salesman_group
    Corrigiendo errores

--- product_salesman_group/user.py
+++ product_salesman_group/user.py
@@ -3,7 +3,7 @@
 # For copyright and license notices, see __openerp__.py file in module root
 # directory
 ##############################################################################
-from openerp import models, api, fields
+from openerp import models, fields
 
 
 class res_users(models.Model):
