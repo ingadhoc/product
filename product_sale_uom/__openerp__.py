26c47b5c2c1d985bca1510c1665797922bbdc242
Juan José Scarafía <scarafia.juanjose@gmail.com>
2016-11-04 14:07:12 -0300

    Update __openerp__.py

--- product_sale_uom/__openerp__.py
+++ product_sale_uom/__openerp__.py
@@ -39,7 +39,4 @@
         'security/ir.model.access.csv',
     ],
     'installable': True,
-    # 'pre_init_hook': 'pre_init_hook',
 }
-
-# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
