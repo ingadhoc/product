a062f19a8d8c1c200a3d7c07f2ebc7022ae14f82
Nicolas Mac Rouillon <nmr@adhoc.com.ar>
2016-11-07 11:55:14 -0300

    MIG sale_stock_product_sale_uom to V9

--- sale_stock_product_sale_uom/sale.py
+++ sale_stock_product_sale_uom/sale.py
@@ -6,9 +6,7 @@
 from openerp import models, api
 
 
-class sale_order_line(models.Model):
-
-    """"""
+class SaleOrderLine(models.Model):
 
     _inherit = 'sale.order.line'
 
@@ -18,11 +16,11 @@ class sale_order_line(models.Model):
             uom=False, qty_uos=0, uos=False, name='', partner_id=False,
             lang=False, update_tax=True, date_order=False, packaging=False,
             fiscal_position=False, flag=False, warehouse_id=False):
-        res = super(sale_order_line, self.with_context(
-                preserve_uom=uom)).product_id_change_with_wh(
-                pricelist, product, qty=qty, uom=uom,
-                qty_uos=qty_uos, uos=uos, name=name, partner_id=partner_id,
-                lang=lang, update_tax=update_tax, date_order=date_order,
-                packaging=packaging, fiscal_position=fiscal_position,
-                flag=flag, warehouse_id=warehouse_id)
+        res = super(SaleOrderLine, self.with_context(
+            preserve_uom=uom)).product_id_change_with_wh(
+            pricelist, product, qty=qty, uom=uom,
+            qty_uos=qty_uos, uos=uos, name=name, partner_id=partner_id,
+            lang=lang, update_tax=update_tax, date_order=date_order,
+            packaging=packaging, fiscal_position=fiscal_position,
+            flag=flag, warehouse_id=warehouse_id)
         return res
