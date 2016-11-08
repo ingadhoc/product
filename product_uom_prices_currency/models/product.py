3dea637a8788774ec11a266daba01c4273d33f09
Emiq2 <eq@adhoc.com.ar>
2016-11-08 16:57:01 -0300

    MIG product_uom_prices_currency

--- product_uom_prices_currency/models/product.py
+++ product_uom_prices_currency/models/product.py
@@ -3,8 +3,8 @@
 # For copyright and license notices, see __openerp__.py file in module root
 # directory
 ##############################################################################
-from openerp import models, fields, _, api
-from openerp.exceptions import Warning
+from openerp import models, fields, api, _
+from openerp.exceptions import UserError
 import openerp.addons.decimal_precision as dp
 import logging
 _logger = logging.getLogger(__name__)
@@ -20,7 +20,7 @@ class ProductSaleUom(models.Model):
         'Price',
         digits=dp.get_precision('Product Price'),
         compute='get_price_on_base_currency'
-        )
+    )
 
     @api.one
     @api.depends('price', 'product_tmpl_id.other_currency_id')
@@ -29,19 +29,17 @@ class ProductSaleUom(models.Model):
         if template.other_currency_id and self.price:
             self.price_on_base_currency = template.other_currency_id.compute(
                 self.price,
-                template.computed_list_price_currency_id,
+                template.currency_id,
                 round=False)
 
 
-class product_template(models.Model):
-
-    """"""
+class ProductTemplate(models.Model):
 
     _inherit = 'product.template'
 
     list_price_type = fields.Selection(
         selection_add=[('by_uom_currency', 'By UOM and Currency')],
-        )
+    )
     other_currency_uom_price_ids = fields.One2many(
         # related='uom_price_ids'
         # we dont use related becuase with onchange it changes this field
@@ -53,22 +51,22 @@ class product_template(models.Model):
         help="Only uoms in this list will be available in sale order lines. "
         "Set a diferent price for this uom. Set the price as 0 and the price "
         "will be calculated as sale price * uom ratio"
-        )
+    )
 
     @api.multi
     def set_prices(self, computed_list_price):
         self.ensure_one()
         if self.list_price_type == 'by_uom_currency' and computed_list_price:
             if not self.other_currency_id:
-                raise Warning(_(
+                raise UserError(_(
                     'You must configure "Other Currency" for product %s' % (
                         self.name)))
             uom_price = self.env['product.sale.uom'].search([
                 ('uom_id', '=', self.uom_id.id),
                 ('product_tmpl_id', '=', self.id),
-                ], limit=1)
+            ], limit=1)
             other_currency_price = (
-                self.computed_list_price_currency_id.compute(
+                self.currency_id.compute(
                     computed_list_price, self.other_currency_id, round=False))
             if uom_price:
                 uom_price.price = other_currency_price
@@ -78,9 +76,9 @@ class product_template(models.Model):
                     'uom_id': self.uom_id.id,
                     'product_tmpl_id': self.id,
                     'price': other_currency_price,
-                    })
+                })
         else:
-            return super(product_template, self).set_prices(
+            return super(ProductTemplate, self).set_prices(
                 computed_list_price)
 
     @api.multi
@@ -91,10 +89,8 @@ class product_template(models.Model):
             if self.other_currency_id and price:
                 return self.other_currency_id.compute(
                     price,
-                    self.computed_list_price_currency_id,
+                    self.currency_id,
                     round=False)
             else:
                 return False
-        return super(product_template, self).get_computed_list_price()
-
-# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
+        return super(ProductTemplate, self).get_computed_list_price()
