5652e42e23ac8a6147c1081bf00bd6ce7f34eb15
Juan Jose Scarafia <jjs@adhoc.com.ar>
2016-12-16 09:45:37 -0300

    ADD migration scripts

---
+++ product_computed_list_price/migrations/9.0.1.0/post-migration.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenUpgrade module for Odoo
+#    @copyright 2015-Today: Odoo Community Association
+#    @author: Stephane LE CORNEC
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+from openupgradelib import openupgrade
+
+
+@openupgrade.migrate(use_env=True)
+def migrate(env, version):
+    # on v8 value was stored on list_price
+    openupgrade.logged_query(env.cr, """
+        UPDATE product_template
+        SET computed_list_price_manual = list_price
+        WHERE list_price_type = 'manual'
+    """,)
