df1d9da3534b77caa980eac529213896ce64d4c8
Juan Jose Scarafia <jjs@adhoc.com.ar>
2016-12-28 10:17:24 -0300

    IMP

--- product_invoice_analysis/models/product_template.py
+++ product_invoice_analysis/models/product_template.py
@@ -15,16 +15,16 @@ class ProductTemplate(models.Model):
         return {
             'type': 'ir.actions.act_window',
             'res_model': 'account.invoice.report',
-            'view_mode': 'pivot,graph',
+            'view_mode': 'tree,pivot,graph',
             # 'view_mode': 'tree,graph',
             'view_type': 'form',
             'domain': [('product_id', 'in', self.product_variant_ids.ids)],
             'context': {
                 # 'search_default_current': 1,
                 # 'search_default_customer': 1,
-                'group_by': [],
-                'group_by_no_leaf': 1,
-                'search_default_year': 1
+                # 'group_by': [],
+                # 'group_by_no_leaf': 1,
+                # 'search_default_year': 1
             }
             # 'context': {
             #     'search_default_product_id': self.product_variant_ids[0].id},
