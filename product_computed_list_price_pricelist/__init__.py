21e182adedd200fb48a00eb120d4b8c44280bb75
Juan Jose Scarafia <jjs@adhoc.com.ar>
2017-02-08 16:44:13 -0300

    DEL product_computed_list_price_pricelist

--- product_computed_list_price_pricelist/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-##############################################################################
-# For copyright and license notices, see __openerp__.py file in module root
-# directory
-##############################################################################
-from . import product
-from . import pricelist
-from .hooks import post_init_hook
