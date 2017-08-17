67b517c11e7995d7bf14130d69dbc6df93175ccd
Juan Jose Scarafia <jjs@adhoc.com.ar>
2017-08-17 17:10:39 -0300

    ADD product_other_sale_description/

---
+++ product_other_sale_description/models/product.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+# For copyright and license notices, see __openerp__.py file in module root
+# directory
+##############################################################################
+from openerp import models, fields
+
+
+class product_template(models.Model):
+    _inherit = 'product.template'
+
+    other_sale_description = fields.Char(
+        'Other Sale Description',
+    )
