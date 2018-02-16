08e814feace73eb299c8169e48c64928a222ae0c
Iván Todorovich <ivan.todorovich@gmail.com>
2018-02-16 12:49:16 -0300

    [IMP][product_replenishment_cost_rule] Rename model file to be consistent

--- product_replenishment_cost_rule/models/__init__.py
+++ product_replenishment_cost_rule/models/__init__.py
@@ -3,5 +3,5 @@
 # For copyright and license notices, see __openerp__.py file in module root
 # directory
 ##############################################################################
-from . import product_product
+from . import product_template
 from . import product_replenishment_cost_rule
