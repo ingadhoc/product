0dbeaa3171b9dba7c801bba56f20a8f01769d605
Juan Jose Scarafia <jjs@adhoc.com.ar>
2018-03-16 13:30:50 -0300

    [IMP] add tracking on product active field

--- product_usability/models/product_product.py
+++ product_usability/models/product_product.py
@@ -3,4 +3,10 @@
 # For copyright and license notices, see __openerp__.py file in module root
 # directory
 ##############################################################################
-from . import models
+from openerp import models, fields
+
+
+class ProductProduct(models.Model):
+    _inherit = 'product.product'
+
+    active = fields.Boolean(track_visibility='onchange')
--- product_usability/models/product_template.py
+++ product_usability/models/product_template.py
@@ -3,4 +3,10 @@
 # For copyright and license notices, see __openerp__.py file in module root
 # directory
 ##############################################################################
-from . import models
+from openerp import models, fields
+
+
+class ProductTemplate(models.Model):
+    _inherit = 'product.template'
+
+    active = fields.Boolean(track_visibility='onchange')
