57a1a27d3597d4b91fea01341ed8e45f12fccba9
nicomacr <nmr@adhoc.com.ar>
2018-04-27 10:00:29 -0300

    [REM] remove create in product.template because not need to set default value in v11

--- product_reference_required/models/product_template.py
+++ product_reference_required/models/product_template.py
@@ -13,17 +13,5 @@ class ProductTemplate(models.Model):
         return self.env['ir.sequence'].next_by_code('product.default_code')
 
     default_code = fields.Char(
-        required=True,
         default=_get_default_code,
     )
-
-    @api.model
-    def create(self, vals):
-        """
-        If we create from template we send default code by context
-        """
-        default_code = vals.get('default_code', False)
-        if default_code:
-            return super(ProductTemplate, self.with_context(
-                default_default_code=default_code)).create(vals)
-        return super(ProductTemplate, self).create(vals)
