505edbaa033dc6aa1750f71bf91542cdbabf0de7
ValentinoDefelice <vd@adhoc.com.ar>
2018-05-10 10:01:48 -0300

    [MIG] product_stock_by_location to V11. (#135)
    * [MIG] product_stock_by_location to V11.
    [FIX] Update 'view' to 'views' names in View Files.
    [FIX] Separate Classes in diferents files (product.product and product.template)
    
    * [FIX] Update README
    
    * [FIX] Not editable pop-up form view

--- product_stock_by_location/models/product_template.py
+++ product_stock_by_location/models/product_template.py
@@ -35,25 +35,6 @@ class ProductTemplate(models.Model):
             'view_mode': 'form',
             'res_model': self._name,
             'view_id': self.env.ref(view).id,
-            'type': 'ir.actions.act_window',
-        }
-
-
-class ProductProduct(models.Model):
-    _inherit = 'product.product'
-
-    @api.multi
-    def view_stock_detail(self):
-        self.ensure_one()
-        view = (
-            'product_stock_by_location.view_product_stock_by_location_form')
-        return {
-            'name': _('Stock By Locations'),
-            'target': 'new',
-            'res_id': self.id,
-            'view_type': 'form',
-            'view_mode': 'form',
-            'res_model': self._name,
-            'view_id': self.env.ref(view).id,
-            'type': 'ir.actions.act_window',
+            'flags': {'initial_mode': 'view'},
+            'type': 'ir.actions.act_window'
         }
