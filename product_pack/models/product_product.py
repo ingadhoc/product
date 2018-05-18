23025084507ba882f1d29a9515566cc1f2c9f8b9
nicomacr <nmr@adhoc.com.ar>
2018-05-18 08:27:22 -0300

    [REL] migration product_pack to 11.0 * [FIX] Update README * [FIX] remove openerp tags and data tags * [IMP] change some funtion to improve funcionality * [FIX] change name to xml and .py files according guidelines * [FIX] remove api.one decorators

--- product_pack/models/product_product.py
+++ product_pack/models/product_product.py
@@ -1,9 +1,8 @@
-# -*- encoding: utf-8 -*-
 ##############################################################################
-# For copyright and license notices, see __manifest__.py file in root directory
+# For copyright and license notices, see __manifest__.py file in module root
+# directory
 ##############################################################################
 from odoo import fields, models, api, _
-from odoo.osv import fields as old_fields
 from odoo.exceptions import UserError
 import math
 
@@ -24,33 +23,24 @@ class ProductProduct(models.Model):
         help='List of packs where product is used.'
     )
 
-    def _product_available(
-            self, cr, uid, ids, field_names=None, arg=False, context=None):
-        """
-        For product packs we get availability in a different way
-        """
-        pack_product_ids = self.search(cr, uid, [
-            ('pack', '=', True),
-            ('id', 'in', ids),
-        ])
-        res = super(ProductProduct, self)._product_available(
-            cr, uid, list(set(ids) - set(pack_product_ids)),
-            field_names, arg, context)
-        for product in self.browse(cr, uid, pack_product_ids, context=context):
+    @api.depends('stock_quant_ids', 'stock_move_ids')
+    def _compute_quantities(self):
+        packs = self.filtered('pack')
+        no_packs = (self + packs.mapped('pack_line_ids.product_id')) - packs
+        super(ProductProduct, no_packs)._compute_quantities()
+        for product in packs:
             pack_qty_available = []
             pack_virtual_available = []
             for subproduct in product.pack_line_ids:
-                subproduct_stock = self._product_available(
-                    cr, uid, [subproduct.product_id.id], field_names, arg,
-                    context)[subproduct.product_id.id]
+                subproduct_stock = subproduct.product_id
                 sub_qty = subproduct.quantity
                 if sub_qty:
                     pack_qty_available.append(math.floor(
-                        subproduct_stock['qty_available'] / sub_qty))
+                        subproduct_stock.qty_available / sub_qty))
                     pack_virtual_available.append(math.floor(
-                        subproduct_stock['virtual_available'] / sub_qty))
+                        subproduct_stock.virtual_available / sub_qty))
             # TODO calcular correctamente pack virtual available para negativos
-            res[product.id] = {
+            vals = {
                 'qty_available': (
                     pack_qty_available and min(pack_qty_available) or False),
                 'incoming_qty': 0,
@@ -59,31 +49,8 @@ class ProductProduct(models.Model):
                     pack_virtual_available and
                     max(min(pack_virtual_available), 0) or False),
             }
-        return res
-
-    def _search_product_quantity(self, cr, uid, obj, name, domain, context):
-        """
-        We use original search function
-        """
-        return super(ProductProduct, self)._search_product_quantity(
-            cr, uid, obj, name, domain, context)
-
-    # overwrite ot this fields so that we can modify _product_available
-    # function to support packs
-    _columns = {
-        'qty_available': old_fields.function(
-            _product_available, multi='qty_available',
-            fnct_search=_search_product_quantity),
-        'virtual_available': old_fields.function(
-            _product_available, multi='qty_available',
-            fnct_search=_search_product_quantity),
-        'incoming_qty': old_fields.function(
-            _product_available, multi='qty_available',
-            fnct_search=_search_product_quantity),
-        'outgoing_qty': old_fields.function(
-            _product_available, multi='qty_available',
-            fnct_search=_search_product_quantity),
-    }
+            product.update(vals)
+        # return res
 
     @api.multi
     @api.constrains('pack_line_ids')
@@ -100,116 +67,23 @@ class ProductProduct(models.Model):
                         'Product id: %s') % rec.id)
                 pack_lines = pack_lines.mapped('product_id.pack_line_ids')
 
-
-class ProductTemplate(models.Model):
-    _inherit = 'product.template'
-
-    # TODO rename a pack_type
-    pack_price_type = fields.Selection([
-        ('components_price', 'Detailed - Components Prices'),
-        ('totalice_price', 'Detailed - Totaliced Price'),
-        ('fixed_price', 'Detailed - Fixed Price'),
-        ('none_detailed_assited_price', 'None Detailed - Assisted Price'),
-        ('none_detailed_totaliced_price', 'None Detailed - Totaliced Price'),
-    ],
-        'Pack Type',
-        help="* Detailed - Components Prices: Detail lines with prices on "
-        "sales order.\n"
-        "* Detailed - Totaliced Price: Detail lines on sales order totalicing "
-        "lines prices on pack (don't show component prices).\n"
-        "* Detailed - Fixed Price: Detail lines on sales order and use product"
-        " pack price (ignore line prices).\n"
-        "* None Detailed - Assisted Price: Do not detail lines on sales "
-        "order. Assist to get pack price using pack lines."
-    )
-    pack = fields.Boolean(
-        'Pack?',
-        help='Is a Product Pack?',
-    )
-    pack_line_ids = fields.One2many(
-        related='product_variant_ids.pack_line_ids'
-    )
-    used_pack_line_ids = fields.One2many(
-        related='product_variant_ids.used_pack_line_ids'
-    )
-
     @api.multi
-    @api.constrains(
-        'product_variant_ids', 'pack_price_type')
-    def check_relations(self):
-        """
-        Check assited packs dont have packs a childs
-        """
-        # check assited price has no packs child of them
-        for rec in self:
-            if rec.pack_price_type == 'none_detailed_assited_price':
-                child_packs = rec.mapped(
-                    'pack_line_ids.product_id').filtered('pack')
-                if child_packs:
-                    raise UserError(_(
-                        'A "None Detailed - Assisted Price Pack" can not have '
-                        'a pack as a child!'))
-
-        # TODO we also should check this
-        # check if we are configuring a pack for a product that is partof a
-        # assited pack
-        # if self.pack:
-        #     for product in self.product_variant_ids
-        #     parent_assited_packs = self.env['product.pack.line'].search([
-        #         ('product_id', '=', self.id),
-        #         ('parent_product_id.pack_price_type', '=',
-        #             'none_detailed_assited_price'),
-        #         ])
-        #     print 'parent_assited_packs', parent_assited_packs
-        #     if parent_assited_packs:
-        #         raise Warning(_(
-        #             'You can not set this product as pack because it is part'
-        #             ' of a "None Detailed - Assisted Price Pack"'))
-
-    @api.multi
-    @api.constrains('company_id', 'product_variant_ids')
-    def check_pack_line_company(self):
-        """
-        Check packs are related to packs of same company
-        """
-        for rec in self:
-            for line in rec.pack_line_ids:
-                if line.product_id.company_id != rec.company_id:
-                    raise UserError(_(
-                        'Pack lines products company must be the same as the '
-                        'parent product company'))
-            for line in rec.used_pack_line_ids:
-                if line.parent_product_id.company_id != rec.company_id:
-                    raise UserError(_(
-                        'Pack lines products company must be the same as the '
-                        'parent product company'))
-
-    @api.multi
-    def write(self, vals):
-        """
-        We remove from prod.prod to avoid error
-        """
-        if vals.get('pack_line_ids', False):
-            self.product_variant_ids.write(
-                {'pack_line_ids': vals.pop('pack_line_ids')})
-        return super(ProductTemplate, self).write(vals)
-
-    @api.model
-    def _price_get(self, products, ptype='list_price'):
-        res = super(ProductTemplate, self)._price_get(
-            products, ptype=ptype)
-        for product in products:
-            if (
-                    product.pack and
-                    product.pack_price_type in [
-                        'totalice_price',
-                        'none_detailed_assited_price',
-                        'none_detailed_totaliced_price']):
-                pack_price = 0.0
-                for pack_line in product.pack_line_ids:
-                    product_line_price = pack_line.product_id.price_get()[
-                        pack_line.product_id.id] * (
-                        1 - (pack_line.discount or 0.0) / 100.0)
-                    pack_price += (product_line_price * pack_line.quantity)
-                res[product.id] = pack_price
-        return res
+    def price_compute(self, price_type, uom=False, currency=False,
+                      company=False):
+        packs = self.filtered(lambda p: p.pack and p.pack_price_type in [
+            'totalice_price',
+            'none_detailed_assited_price',
+            'none_detailed_totaliced_price',
+        ])
+        no_packs = (self | self.mapped('pack_line_ids.product_id')) - packs
+        prices = super(ProductProduct, no_packs).price_compute(
+            price_type, uom, currency, company)
+        for product in packs:
+            pack_price = 0.0
+            for pack_line in product.pack_line_ids:
+                product_line_price = prices[
+                    pack_line.product_id.id] * (
+                    1 - (pack_line.discount or 0.0) / 100.0)
+                pack_price += (product_line_price * pack_line.quantity)
+            prices[product.id] = pack_price
+        return prices
