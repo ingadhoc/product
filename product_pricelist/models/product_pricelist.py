47d999c6e743e69b2c6628ff87b9e5e679f2fd95
nicomacr <nmr@adhoc.com.ar>
2018-05-29 08:59:30 -0300

    [FIX] remove comment lines and some other code fixes

--- product_pricelist/models/product_pricelist.py
+++ product_pricelist/models/product_pricelist.py
@@ -10,7 +10,6 @@ class ProductPricelist(models.Model):
 
     price = fields.Monetary(
         compute='_compute_price',
-        # digits=dp.get_precision('Product Price'),
         help='Price for product specified on the context',
     )
     show_products = fields.Boolean(
@@ -20,9 +19,9 @@ class ProductPricelist(models.Model):
 
     @api.multi
     def _compute_price(self):
+        product_id = self._context.get('product_id', False)
+        template_id = self._context.get('template_id', False)
         for rec in self:
-            product_id = self._context.get('product_id', False)
-            template_id = self._context.get('template_id', False)
             if product_id:
                 price = self.env['product.product'].browse(
                     product_id).with_context(pricelist=rec.id).price
@@ -31,73 +30,3 @@ class ProductPricelist(models.Model):
                 price = self.env['product.template'].browse(
                     template_id).with_context(pricelist=rec.id).price
                 rec.price = price
-
-    # not yet implemented
-    # @api.multi
-    # def action_related_pricelist_items(self):
-    #     self.ensure_one()
-    #     product_id = self._context.get('product_id', False)
-    #     template_id = self._context.get('template_id', False)
-    #     onle_related_product_items = self._context.get(
-    #         'onle_related_product_items', False)
-    #     domain = [
-    #         ('price_version_id.pricelist_id', '=', self.id),
-    #     ]
-    #     if onle_related_product_items:
-    #         if product_id:
-    #             product = self.env['product.product'].browse(product_id)
-    #             domain += [
-    #                 '|',
-    #                 '|',
-    #                 '|',
-    #                 ('product_id', '=', product.id),
-    #                 '&',
-    #                 ('product_id', '=', False),
-    #                 ('product_tmpl_id', '=', product.product_tmpl_id.id),
-    #                 '&',
-    #                 ('product_id', '=', False),
-    #                 '&',
-    #                 ('product_tmpl_id', '=', False),
-    #                 ('categ_id', '=', product.categ_id.id),
-    #                 '&',
-    #                 ('product_id', '=', False),
-    #                 '&',
-    #                 ('product_tmpl_id', '=', False),
-    #                 ('categ_id', '=', False),
-    #             ]
-    #         elif template_id:
-    #             template = self.env['product.template'].browse(template_id)
-    #             domain += [
-    #                 '|',
-    #                 '|',
-    #                 '&',
-    #                 ('product_id', '=', False),
-    #                 ('product_tmpl_id', '=', template.id),
-    #                 '&',
-    #                 ('product_id', '=', False),
-    #                 '&',
-    #                 ('product_tmpl_id', '=', False),
-    #                 ('categ_id', '=', template.categ_id.id),
-    #                 '&',
-    #                 ('product_id', '=', False),
-    #                 '&',
-    #                 ('product_tmpl_id', '=', False),
-    #                 ('categ_id', '=', False),
-    #             ]
-    #     related_items = self.env['product.pricelist.item'].search(domain)
-    #     name = _('Pricelist Items')
-    #     new_context = self._context.copy()
-    #     new_context['show_version'] = True
-    #     new_context['pricelist_id'] = self.id
-    #     res = {
-    #         'name': name,
-    #         'view_mode': 'tree,form',
-    #         'view_type': 'form',
-    #         'res_model': 'product.pricelist.item',
-    #         'type': 'ir.actions.act_window',
-    #         'nodestroy': True,
-    #         'target': 'current',
-    #         'domain': [('id', 'in', related_items.ids)],
-    #         'context': new_context,
-    #     }
-    #     return res
--- product_pricelist/models/product_template.py
+++ product_pricelist/models/product_template.py
@@ -8,27 +8,14 @@ from odoo import models, fields, api
 class ProductTemplate(models.Model):
     _inherit = 'product.template'
 
-    # product_id = fields.Integer(
-    #     related='product_variant_ids.product_id'
-    #     # 'product.pricelist',
-    #     # compute='_get_product_id',
-    #     # store=True
-    #     )
-    # pricelist_ids = fields.Many2many(
     pricelist_ids = fields.One2many(
         'product.pricelist',
         compute='_compute_pricelist_ids',
-        # inverse='dummy_inverse',
         string='Pricelists',
     )
-
-    # not implemented yet
-    # @api.one
-    # def dummy_inverse(self):
-    #     return True
 
     @api.multi
     def _compute_pricelist_ids(self):
         for rec in self:
-            self.pricelist_ids = self.pricelist_ids.search(
+            rec.pricelist_ids = rec.pricelist_ids.search(
                 [('show_products', '=', True)])
