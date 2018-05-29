934a477b0d193c4604f7b804882937d6b8117bec
ValentinoDefelice <vd@adhoc.com.ar>
2018-05-29 11:49:33 -0300

    [REM] product_computed_price_rule deprecated for V11. (#152)

--- product_computed_price_rule/models/__init__.py
+++
@@ -1,6 +0,0 @@
-##############################################################################
-# For copyright and license notices, see __manifest__.py file in module root
-# directory
-##############################################################################
-from . import product_product
-from . import computed_list_price_cost_rule
