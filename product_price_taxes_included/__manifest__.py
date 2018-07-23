87324d8105a5c722694e286844655f38822e46ea
Katherine Zaoral <zaoral@users.noreply.github.com>
2018-07-23 09:18:57 -0300

    [REL] product_price_taxes_included: Migration to version 11.0 (#147)
    * [ADD] product_price_taxes_included: update module version
    
    * [ADD] product_price_taxes_included: set module installable
    
    [ADD] product_price_taxes_included: remove deprecated test key from manifest
    
    * [FIX] product_price_taxes_included: remove execution permissions from files
    
    * [ADD] product_price_taxes_included: separate models per file
    
    * [ADD] product_price_taxes_included: rename file to follow guideline
    
    [ADD] product_price_taxes_included: remove no needed imports
    
    * [IMP] product_price_taxes_included: Update README
    
    - Update using the new guideline template.
    - Fix some misspelling errors.
    
    * [IMP] product_price_taxes_included: Fix and Improves models
    
    - fix misspelling error in class definition
    - set important comments as method docstrings.
    - fix pylint error not used variable
    - simplify condition on if.
    - fix pylint bad identation
    - update product.product compute and inverse method to use new api
    - adapt product.template to new api.
    
    * [FIX] product_price_taxes_included: Fix views
    
    - rename view file to follow guideline.
    - fix error in the xml do not load in odoo
    - replace openerp tag with odoo
    - remove deprecated data tag
    
    * [IMP] release of product_price_taxes_included to v11

--- product_price_taxes_included/__manifest__.py
+++ product_price_taxes_included/__manifest__.py
@@ -19,7 +19,7 @@
 ##############################################################################
 {
     'name': 'Product Price Taxes Included or Not',
-    'version': '9.0.1.0.0',
+    'version': '11.0.1.0.0',
     'category': 'Product',
     'sequence': 14,
     'summary': '',
@@ -32,13 +32,12 @@
         'account',
     ],
     'data': [
-        'views/product_view.xml',
+        'views/product_template_views.xml',
+        'views/product_product_views.xml',
     ],
     'demo': [
     ],
-    'test': [
-    ],
-    'installable': False,
+    'installable': True,
     'auto_install': False,
     'application': False,
 }
--- product_price_taxes_included/models/__init__.py
+++ product_price_taxes_included/models/__init__.py
@@ -3,5 +3,6 @@
 # directory
 ##############################################################################
 
-from . import product
-from . import pricelist
+from . import product_template
+from . import product_product
+from . import product_pricelist
