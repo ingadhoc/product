c676d3c17596b50d592daa3f2cb6207c9cb14143
nicomacr <nmr@adhoc.com.ar>
2018-09-05 11:07:55 -0300

    [ADD] Add products Uoms moudule in 11.0 replacing product_sale_uom

---
+++ product_uoms/models/__init__.py
@@ -0,0 +1,7 @@
+##############################################################################
+# For copyright and license notices, see __manifest__.py file in module root
+# directory
+##############################################################################
+from . import product_uoms
+from . import product_product
+from . import product_template
