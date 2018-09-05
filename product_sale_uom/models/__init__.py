555cdcab45cfa50a751bdf94dccdda0f574c4ac2
nicomacr <nmr@adhoc.com.ar>
2018-09-05 11:05:44 -0300

    [REM] rename product_sale_uom in other modules

--- product_sale_uom/models/__init__.py
+++
@@ -1,7 +0,0 @@
-##############################################################################
-# For copyright and license notices, see __manifest__.py file in module root
-# directory
-##############################################################################
-from . import product
-from . import sale
-
