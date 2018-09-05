3082309d51a8d856069f0e0214526233a96fcd5a
nicomacr <nmr@adhoc.com.ar>
2018-09-05 11:13:51 -0300

    [ADD] add product_uoms_stock to 11.0 not instable for now

---
+++ product_uoms_stock/models/product_uoms.py
@@ -0,0 +1,11 @@
+##############################################################################
+# For copyright and license notices, see __manifest__.py file in module root
+# directory
+##############################################################################
+from odoo import models, fields
+
+
+class ProductUoms(models.Model):
+    _inherit = 'product.uoms'
+
+    stock_ok = fields.Boolean()
