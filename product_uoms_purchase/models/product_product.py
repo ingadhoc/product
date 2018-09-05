5a25a4b86931a799907d4efe8875fa1d0b8efb54
nicomacr <nmr@adhoc.com.ar>
2018-09-05 11:11:53 -0300

    [ADD] add product_uoms_purchase module to 11.0 replacing product_sale_uom

---
+++ product_uoms_purchase/models/product_product.py
@@ -0,0 +1,16 @@
+##############################################################################
+# For copyright and license notices, see __manifest__.py file in module root
+# directory
+##############################################################################
+from odoo import models, api
+
+
+class ProductProduct(models.Model):
+    _inherit = 'product.product'
+
+    @api.multi
+    def get_product_uoms(self, product_uom, use=False):
+        if use == 'purchase' and self.uom_ids.filtered('purchase_ok'):
+            return self.uom_ids.filtered('purchase_ok').mapped('uom_id')
+        return super(ProductProduct, self).get_product_uoms(
+            product_uom, use=use)
