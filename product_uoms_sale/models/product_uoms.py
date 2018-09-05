dfa97deb8157d03b6cc0427765346f5d5e130962
nicomacr <nmr@adhoc.com.ar>
2018-09-05 11:10:54 -0300

    [ADD] add product_uoms_sale module to 11.0 replacing product_sale_uom

---
+++ product_uoms_sale/models/product_uoms.py
@@ -0,0 +1,13 @@
+##############################################################################
+# For copyright and license notices, see __manifest__.py file in module root
+# directory
+##############################################################################
+from odoo import models, fields
+
+
+class ProductUoms(models.Model):
+    _inherit = 'product.uoms'
+
+    sale_ok = fields.Boolean(
+        default=True,
+    )
---
+++ product_uoms_sale/models/sale_order_line.py
@@ -0,0 +1,55 @@
+##############################################################################
+# For copyright and license notices, see __manifest__.py file in module root
+# directory
+##############################################################################
+from odoo import models, fields, api, _
+from odoo.exceptions import ValidationError
+
+
+class SaleOrderLine(models.Model):
+
+    _inherit = 'sale.order.line'
+
+    uom_unit_ids = fields.Many2many(
+        'product.uom',
+        compute='_compute_uom_unit',
+    )
+
+    @api.depends('product_id')
+    def _compute_uom_unit(self):
+        for rec in self.filtered('product_id'):
+            rec.uom_unit_ids = rec.product_id.get_product_uoms(
+                rec.product_id.uom_id, use='sale')
+
+    @api.onchange('product_id')
+    def product_id_change(self):
+        product_uom = None
+        product_uom_domain = None
+        if self.product_id:
+            product = self.product_id
+            sale_product_uoms = product.get_product_uoms(
+                product.uom_id, use='sale')
+            if sale_product_uoms:
+                product_uom = sale_product_uoms[0].id
+                # we do this because odoo overwrite view domain
+                product_uom_domain = [('id', 'in', sale_product_uoms.ids)]
+        res = super(SaleOrderLine, self).product_id_change()
+        if product_uom:
+            self.product_uom = product_uom
+        if product_uom_domain:
+            res = {'domain': {'product_uom': product_uom_domain}}
+        return res
+
+    @api.constrains('product_uom')
+    def check_uoms(self):
+        for rec in self:
+            product = rec.product_id
+            sale_product_uoms = product.get_product_uoms(
+                product.uom_id, use='sale')
+            if rec.product_uom not in sale_product_uoms:
+                raise ValidationError(_(
+                    "%s unit of measure is not valid for sale,"
+                    " please change %s product line with the proper"
+                    " uom in order to continue" % (
+                        rec.product_uom.name, product.display_name,
+                    )))
