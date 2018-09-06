f08cc84ba3fe4fbddfd4d47da044175e68e81406
nicomacr <nmr@adhoc.com.ar>
2018-09-06 08:01:07 -0300

    Revert "[ADD] add product_uoms_stock to 11.0 not instable for now"
    This reverts commit 9d3df06c2e34914c44aeb719f48b8841d28ff526.

--- product_uoms_stock/models/__init__.py
+++
@@ -1,9 +0,0 @@
-##############################################################################
-# For copyright and license notices, see __manifest__.py file in module root
-# directory
-##############################################################################
-from . import product_product
-from . import product_uoms
-from . import stock_move
-from . import stock_move_line
-
