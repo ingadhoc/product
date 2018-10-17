497e0bb2f014dd36681344720cadf39c4d845497
Nicolas Mac Rouillon <nicomacr@users.noreply.github.com>
2018-10-17 17:48:34 -0300

    [FIX] validate if property_payment_term_id is setting when evaluate the constrains (#177)

--- price_security/models/res_partner.py
+++ price_security/models/res_partner.py
@@ -9,14 +9,23 @@ from odoo.exceptions import ValidationError
 class ResPartner(models.Model):
     _inherit = 'res.partner'
 
-    @api.constrains('property_product_pricelist')
-    def check_property_product_pricelist(self):
-        if self.user_has_groups('price_security.group_restrict_prices'):
+    @api.one
+    def _inverse_product_pricelist(self):
+        """ if user is in group restrict prices we still allow him to create
+        partners without pricelist or to delete pricelist from partners"""
+        if self.user_has_groups('price_security.group_restrict_prices') and \
+                self.property_product_pricelist:
             raise ValidationError(_(
                 'You are not allowed to change the pricelist'))
+        return super(ResPartner, self)._inverse_product_pricelist()
 
     @api.constrains('property_payment_term_id')
     def check_property_payment_term_id(self):
-        if self.user_has_groups('price_security.group_restrict_prices'):
+        """ if user is in group restrict prices we still allow him to create
+        partners without payment term or to delete payment term from
+        partners"""
+        if self.user_has_groups(
+                'price_security.group_restrict_prices') and \
+                self.filtered(lambda x: x.property_payment_term_id):
             raise ValidationError(_(
                 'You are not allowed to change the Payment Term'))
