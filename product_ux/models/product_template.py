faf603970ece19d1313bf724e2091d8bd504fcfe
nicomacr <nmr@adhoc.com.ar>
2019-01-18 11:15:10 -0300

    [IMP] new field user_company_currency_id to product

--- product_ux/models/product_template.py
+++ product_ux/models/product_template.py
@@ -2,7 +2,7 @@
 # For copyright and license notices, see __manifest__.py file in module root
 # directory
 ##############################################################################
-from odoo import models, fields
+from odoo import models, fields, api
 
 
 class ProductTemplate(models.Model):
@@ -17,3 +17,12 @@ class ProductTemplate(models.Model):
     )
 
     warranty = fields.Float()
+    user_company_currency_id = fields.Many2one(
+        'res.currency',
+        compute='_compute_user_company_currency_id',
+    )
+
+    @api.depends()
+    def _compute_user_company_currency_id(self):
+        for rec in self:
+            rec.user_company_currency_id = self.env.user.company_id.currency_id
