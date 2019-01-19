5093aba95c9cdb09ddf1b20c4204ba862e13acd8
Juan Jose Scarafia <jjs@adhoc.com.ar>
2019-01-19 14:35:03 -0300

    [REF] remove prints

--- product_currency/migrations/11.0.1.1.0/post-migration.py
+++ product_currency/migrations/11.0.1.1.0/post-migration.py
@@ -5,13 +5,11 @@ from openupgradelib import openupgrade
 def migrate(env, version):
     companies = env['res.company'].search([])
     for company in companies:
-        print ('company', company)
         for product in env['product.product'].with_context(
                 force_company=company.id).search([
                     ('force_currency_id', '!=', company.currency_id.id),
                     ('force_currency_id', '!=', False),
                 ]):
-            print ('product', product)
             standard_price = product.currency_id.compute(
                 product.standard_price, company.currency_id)
             product.standard_price = standard_price
