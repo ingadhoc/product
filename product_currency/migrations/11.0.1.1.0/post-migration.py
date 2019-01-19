bcd8dab780e309297e126a8b482067bf0b38ef69
Juan Jose Scarafia <jjs@adhoc.com.ar>
2019-01-19 14:29:07 -0300

    [IMP] migration scripts performance

--- product_currency/migrations/11.0.1.1.0/post-migration.py
+++ product_currency/migrations/11.0.1.1.0/post-migration.py
@@ -4,11 +4,14 @@ from openupgradelib import openupgrade
 @openupgrade.migrate(use_env=True)
 def migrate(env, version):
     companies = env['res.company'].search([])
-    products = env['product.product'].search([])
     for company in companies:
-        for product in products.filtered(
-                lambda x: x.currency_id != company.currency_id):
-            standard_price = product.currency_id.compute(product.with_context(
-                force_company=company.id).standard_price, company.currency_id)
-            product.with_context(
-                force_company=company.id).standard_price = standard_price
+        print ('company', company)
+        for product in env['product.product'].with_context(
+                force_company=company.id).search([
+                    ('force_currency_id', '!=', company.currency_id.id),
+                    ('force_currency_id', '!=', False),
+                ]):
+            print ('product', product)
+            standard_price = product.currency_id.compute(
+                product.standard_price, company.currency_id)
+            product.standard_price = standard_price
