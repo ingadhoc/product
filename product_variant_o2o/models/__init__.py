2dc17c3575bd31205feda7cfd5f5577aea0980e3
nicomacr <nmr@adhoc.com.ar>
2019-03-22 11:29:00 -0300

    [FIX] change the order of files .py in init to solve an error when installing

--- product_variant_o2o/models/__init__.py
+++ product_variant_o2o/models/__init__.py
@@ -2,6 +2,6 @@
 # For copyright and license notices, see __manifest__.py file in module root
 # directory
 ##############################################################################
+from . import product_attribute
 from . import product_template
 from . import product_product
-from . import product_attribute
