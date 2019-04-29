2a0e3da11f51fc78b125518df7623efb54134e7c
Valentino <vd@adhoc.com.ar>
2019-04-29 10:14:07 -0300

    [ADD] product_attribute_template module

---
+++ product_attribute_template/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'Product Attribute Template',
+    'version': '11.0.1.0.0',
+    'category': 'Sales Management',
+    'author': 'ADHOC SA, Odoo Community Association (OCA)',
+    'website': 'http://www.adhoc.com.ar/',
+    'license': 'AGPL-3',
+    'depends': [
+        'product',
+    ],
+    'data': [
+        'views/product_attribute_template_views.xml',
+        'views/product_template_views.xml',
+    ],
+    'installable': True,
+}
