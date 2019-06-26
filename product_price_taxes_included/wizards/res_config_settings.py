c08318dc10e06b89e495c1289918f58d7b9abf79
Nicolás Mac Rouillon <nmr@adhoc.com.ar>
2019-06-26 10:41:42 -0300

    [ADD] product_price_taxes_included: Implement new config in sales
    To show the price with taxes in kanban of product template, now set a boolean in sales config to set the user to a new group. This activate a view for this group to do that.

---
+++ product_price_taxes_included/wizards/res_config_settings.py
@@ -0,0 +1,15 @@
+##############################################################################
+# For copyright and license notices, see __manifest__.py file in module root
+# directory
+##############################################################################
+from odoo import models, fields
+
+
+class ResConfigSettings(models.TransientModel):
+
+    _inherit = 'res.config.settings'
+
+    group_kanban_prices_with_tax = fields.Boolean(
+        "Price with taxes in kanban view",
+        implied_group='product_price_taxes_included.group_prices_with_tax',
+    )
