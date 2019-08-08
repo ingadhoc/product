d8eba5f94631e5e8da99f807c96ead694ef89dc2
Lucas L. Soto <ls@adhoc.com.ar>
2019-08-08 20:37:58 -0300

    [MIG] product_uoms_sale: write short supers and make installable
    * [MIG] product_uoms_sale: write short supers and make installable
    
    - set module installable
    - change references to product.uom to uom.uom new model
    - make shoert supers
    
    * [ADD] product_uoms_sale: improve get_product_uoms method, remove not needed argument
    
    * [MIG] product_uoms: make installable and several changes
    
    - set product_uoms module installable
    - fix reference to odoo's group that was renamed in version 12.0
    - get_product_uoms method improve, now we dont have to pass the uom record as argument
    - remove readonly fro related fild, this one is already readonly by default.
    - change references told model product.uom, now renamed to uom.uom
    - fix misspelling error in security of the module

--- product_uoms_sale/models/sale_order_line.py
+++ product_uoms_sale/models/sale_order_line.py
@@ -1,4 +1,4 @@
-##############################################################################
+
 # For copyright and license notices, see __manifest__.py file in module root
 # directory
 ##############################################################################
@@ -11,15 +11,14 @@ class SaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
 
     uom_unit_ids = fields.Many2many(
-        'product.uom',
+        'uom.uom',
         compute='_compute_uom_unit',
     )
 
     @api.depends('product_id')
     def _compute_uom_unit(self):
         for rec in self.filtered('product_id'):
-            rec.uom_unit_ids = rec.product_id.get_product_uoms(
-                rec.product_id.uom_id, use='sale')
+            rec.uom_unit_ids = rec.product_id.get_product_uoms(use='sale')
 
     @api.onchange('product_id')
     def product_id_change(self):
@@ -27,13 +26,12 @@ class SaleOrderLine(models.Model):
         product_uom_domain = None
         if self.product_id:
             product = self.product_id
-            sale_product_uoms = product.get_product_uoms(
-                product.uom_id, use='sale')
+            sale_product_uoms = product.get_product_uoms(use='sale')
             if sale_product_uoms:
                 product_uom = sale_product_uoms[0].id
                 # we do this because odoo overwrite view domain
                 product_uom_domain = [('id', 'in', sale_product_uoms.ids)]
-        res = super(SaleOrderLine, self).product_id_change()
+        res = super().product_id_change()
         if product_uom:
             self.product_uom = product_uom
         if product_uom_domain:
@@ -44,8 +42,7 @@ class SaleOrderLine(models.Model):
     def check_uoms(self):
         for rec in self:
             product = rec.product_id
-            sale_product_uoms = product.get_product_uoms(
-                product.uom_id, use='sale')
+            sale_product_uoms = product.get_product_uoms(use='sale')
             if rec.product_uom not in sale_product_uoms:
                 raise ValidationError(_(
                     "%s unit of measure is not valid for sale,"
