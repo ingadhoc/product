b1b69c36a6b73697e0897061aebee737f3f30baa
Lucas L. Soto <ls@adhoc.com.ar>
2019-08-21 17:05:24 -0300

    [REL] product_reference_required: first release 12.0 (#244)
    - Make installable.
    - Optimize code.

--- product_reference_required/models/product_template.py
+++ product_reference_required/models/product_template.py
@@ -2,16 +2,13 @@
 # For copyright and license notices, see __manifest__.py file in module root
 # directory
 ##############################################################################
-from odoo import models, fields, api
+from odoo import models, fields
 
 
 class ProductTemplate(models.Model):
     _inherit = "product.template"
 
-    @api.model
-    def _get_default_code(self):
-        return self.env['ir.sequence'].next_by_code('product.default_code')
-
     default_code = fields.Char(
-        default=_get_default_code,
+        default=lambda self: self.env['ir.sequence'].next_by_code(
+            'product.default_code'),
     )
