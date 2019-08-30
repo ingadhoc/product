6085f3253365624d0acff80862fcb9ae12657073
Juan José Scarafía <jjs@adhoc.com.ar>
2019-08-30 13:54:05 -0300

    [REL] product_prices_update on v11 (#247)
    * Revert "[REM] product_prices_update deprecated for version 11.0 (#127)"
    
    This reverts commit b822b3e0728b68368861aede1108f6dceb73cfae.
    
    * [REL] product_prices_update on v11

---
+++ product_prices_update/__init__.py
@@ -0,0 +1,6 @@
+##############################################################################
+# For copyright and license notices, see __manifest__.py file in module root
+# directory
+##############################################################################
+from . import wizard
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
