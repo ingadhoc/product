2b0d4ac2a6b34170545d97a35a3a3cb1e3a15c3f
Nicolás Mac Rouillon <nmr@adhoc.com.ar>
2019-09-25 12:05:01 -0300

    [REF] price_security: Remove ugly way to write values
    In this case the implementation to save values in fields readonly on tree views are solved by the attribute "force_save" in the field insted a ugly way using realted fields and views to do this.

--- price_security/models/account_invoice_line.py
+++ price_security/models/account_invoice_line.py
@@ -9,29 +9,10 @@ from odoo.tools import float_compare
 class AccountInvoiceLine(models.Model):
     _inherit = 'account.invoice.line'
 
-    # we add this fields instead of making original readonly because we need
-    # on change to change values, we make readonly in view because sometimes
-    # we want them to be writeable
-    invoice_line_tax_ids_readonly = fields.Many2many(
-        related='invoice_line_tax_ids',
-        readonly=True,
-    )
-    price_unit_readonly = fields.Float(
-        related='price_unit',
-        readonly=True,
-    )
     product_can_modify_prices = fields.Boolean(
         related='product_id.can_modify_prices',
         readonly=True,
         string='Product Can modify prices')
-
-    @api.onchange('price_unit_readonly')
-    def onchange_price_unit_readonly(self):
-        self.price_unit = self.price_unit_readonly
-
-    @api.onchange('invoice_line_tax_ids_readonly')
-    def onchange_invoice_line_tax_id_readonly(self):
-        self.invoice_line_tax_ids = self.invoice_line_tax_ids_readonly
 
     @api.multi
     @api.constrains(
