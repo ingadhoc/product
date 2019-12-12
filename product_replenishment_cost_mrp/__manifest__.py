da9811a4d65aeee011c4b1bf99dcac300455b8e2
Daniel Libonati <dl@adhoc.com.ar>
2019-12-12 16:46:20 -0300

    [REL] ingadhoc/product:product_replenishment_cost_mrp - Migration to v12 (#279)
    * Adapted for using v12 existing MRP report
    
    * Added translation for travis
    
    * Fix long lines

--- product_replenishment_cost_mrp/__manifest__.py
+++ product_replenishment_cost_mrp/__manifest__.py
@@ -1,7 +1,7 @@
 {
     'name': 'Integration between Replenishment Cost and Manufacture',
-    'version': '11.0.1.5.0',
-    'author': "ADHOC SA, Camptocamp,GRAP,Odoo Community Association (OCA)",
+    'version': '12.0.1.0.0',
+    'author': "ADHOC SA, Camptocamp, GRAP, Odoo Community Association (OCA)",
     'license': 'AGPL-3',
     'category': 'Products',
     'depends': [
@@ -10,7 +10,8 @@
     ],
     'website': 'http://www.camptocamp.com/',
     'data': [
-        'report/mrp_bom_replenishment_cost_report_templates.xml',
+        'views/mrp_bom_views.xml',
+        'report/mrp_report_bom_structure.xml'
     ],
-    'installable': False,
+    'installable': True,
 }
--- product_replenishment_cost_mrp/models/product_template.py
+++ product_replenishment_cost_mrp/models/product_template.py
@@ -1,4 +1,4 @@
-from odoo import models, fields, api
+from odoo import models, fields, api, _
 from odoo.exceptions import UserError
 
 
@@ -12,9 +12,9 @@ class ProductTemplate(models.Model):
     @api.onchange('replenishment_cost_type')
     def onchange_replenishment_cost_type(self):
         if self.replenishment_cost_type == 'bom' and not self._origin.bom_ids:
-            raise UserError(
-                'Si elige tipo de costo de reposición "Basado en Ldm" el '
-                'producto debe tener asociadas listas de materiales')
+            raise UserError(_(
+                'If you choose replenishment cost type "Based on BoM" '
+                'then the product must have a bill of materials.'))
 
     def _compute_replenishment_cost(self):
         bom_costs = self.filtered(lambda x: x.replenishment_cost_type == 'bom')
@@ -31,10 +31,13 @@ class ProductTemplate(models.Model):
             for sbom, sbom_data in result2:
                 # TODO ver si podemos evitar este invalidate cache. Es necesario
                 # para que actualice desde el cron
-                sbom.product_id.product_tmpl_id.invalidate_cache(['replenishment_cost'])
+                sbom.product_id.product_tmpl_id.invalidate_cache(
+                    ['replenishment_cost'])
                 sbom_rep_cost = sbom.product_id.uom_id._compute_price(
-                    sbom.product_id.product_tmpl_id.replenishment_cost, sbom.product_uom_id) * sbom_data['qty']
-                price += sbom.product_id.product_tmpl_id.currency_id.compute(sbom_rep_cost, product_currency, round=False)
+                    sbom.product_id.product_tmpl_id.replenishment_cost,
+                    sbom.product_uom_id) * sbom_data['qty']
+                price += sbom.product_id.product_tmpl_id.currency_id.compute(
+                    sbom_rep_cost, product_currency, round=False)
             # NO implementamos total va a ser borrado. Ver si implementamos mas adelante (tener en cuenta convertir
             # moneda)
             # if bom.routing_id:
@@ -46,7 +49,8 @@ class ProductTemplate(models.Model):
             #         price += bom.product_uom_id._compute_price(total_cost, bom.product_id.uom_id)
             # Convert on product UoM quantities
             if price > 0:
-                price = bom.product_uom_id._compute_price(price / bom.product_qty, rec.uom_id)
+                price = bom.product_uom_id._compute_price(
+                    price / bom.product_qty, rec.uom_id)
 
             replenishment_base_cost_on_currency = replenishment_cost = price
             if rec.replenishment_cost_rule_id:
