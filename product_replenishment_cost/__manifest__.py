f2b386f52ec14aa7e96ec4b6401cf640d33bac1e
Nicolas Mac Rouillon <nicomacr@users.noreply.github.com>
2019-12-26 09:59:14 -0300

    [12.0] [PORT] from 11.0 (#281)
    [FIX] product_replenishment_cost: price precision (#280)
    
    This it's becase a bug that odoo solves in this commit https://github.com/odoo/odoo/commit/cf32a23b92435269035e60fe4ba4b627e42ef8d4

--- product_replenishment_cost/__manifest__.py
+++ product_replenishment_cost/__manifest__.py
@@ -19,7 +19,7 @@
 ##############################################################################
 {
     'name': 'Replenishment Cost',
-    'version': '12.0.1.0.0',
+    'version': '12.0.1.1.0',
     'author': "ADHOC SA, Camptocamp,GRAP,Odoo Community Association (OCA)",
     'license': 'AGPL-3',
     'category': 'Products',
