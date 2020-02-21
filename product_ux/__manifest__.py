1df9ae2cffdb134a4f56bc656e8ec35ef55d497c
Nicolas Mac Rouillon <nicomacr@users.noreply.github.com>
2020-02-21 16:30:02 -0300

    [MIG] product_ux: Migration to 13.0 (#285)
    * [MIG] product_ux: Migration to 13.0
    
    * [FIX] change the position path to the button view items of pricelist.
    
    * [FIX] change track_visibility to tracking.
    [FIX] use the company in the enviroment.

--- product_ux/__manifest__.py
+++ product_ux/__manifest__.py
@@ -19,7 +19,7 @@
 ##############################################################################
 {
     'name': 'Product UX',
-    'version': '12.0.1.0.0',
+    'version': "13.0.1.0.0",
     'category': 'Products',
     'sequence': 14,
     'summary': '',
@@ -40,7 +40,7 @@
     ],
     'demo': [
     ],
-    'installable': False,
+    'installable': True,
     'auto_install': False,
     'application': False,
 }
