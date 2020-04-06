9e81733dfff60ee84c51d5937acdc639c719f261
Nicolás Mac Rouillon <nmr@adhoc.com.ar>
2020-04-06 15:13:53 -0300

    [FIX] product_reference_required: solve an error when create product without secuence
    In this case if you want to not use the secuence, the module add an "/" that cause problems with the rule who can't be use the same reference for more than one product.

--- product_reference_required/models/product_product.py
+++ product_reference_required/models/product_product.py
@@ -10,10 +10,17 @@ class ProductProduct(models.Model):
 
     @api.model
     def _get_default_code(self):
-        return self.env['ir.sequence'].next_by_code(
-            'product.default_code') or '/'
+        return self.env['ir.sequence'].next_by_code('product.default_code')
 
     default_code = fields.Char(
         required=True,
         default=_get_default_code,
     )
+
+    @api.model
+    def create(self, vals):
+        if not vals.get('default_code', False) and vals.get('product_tmpl_id', False):
+            product_tmpl = self.env['product.template'].browse(vals.get('product_tmpl_id'))
+            vals['default_code'] = product_tmpl.default_code or self.env['ir.sequence'].next_by_code(
+                'product.default_code')
+        return super().create(vals)
