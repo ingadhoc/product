d24cca65c54d79f61e12547919b0e23d630f7d45
Nicolás Mac Rouillon <nmr@adhoc.com.ar>
2020-06-09 09:59:26 -0300

    [MIG] product_prices_update: Migration to 13.0
    * [FIX] change the name of the folder wizard to wizards
    * [FIX] use self._context insted variable context
    * [FIX] adapt action view to v13.

--- product_prices_update/__init__.py
+++ product_prices_update/__init__.py
@@ -2,5 +2,4 @@
 # For copyright and license notices, see __manifest__.py file in module root
 # directory
 ##############################################################################
-from . import wizard
-# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
+from . import wizards
