0bb1b7b4feb0cddc6c4469afdaa44c19a87afc6b
Nicolás Mac Rouillon <nmr@adhoc.com.ar>
2020-06-16 09:22:02 -0300

    [13.0] [FIX] product_ux: Remove user_company_currency_id to use the odoo cost currency field

--- product_ux/models/product_template.py
+++ product_ux/models/product_template.py
@@ -16,12 +16,3 @@ class ProductTemplate(models.Model):
     )
 
     warranty = fields.Float()
-    user_company_currency_id = fields.Many2one(
-        'res.currency',
-        compute='_compute_user_company_currency_id',
-    )
-
-    @api.depends()
-    def _compute_user_company_currency_id(self):
-        for rec in self:
-            rec.user_company_currency_id = self.env.company.currency_id
