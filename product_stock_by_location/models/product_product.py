1a337b7343d7dc6be2d8f428cd5ba552e25f2970
Bruno <brunozanotti96@gmail.com>
2020-06-24 13:27:20 -0300

    [REL] product_stock_by_location: Migration to 13.0 * removed unnecessary api import * removed method decimal_precision

--- product_stock_by_location/models/product_product.py
+++ product_stock_by_location/models/product_product.py
@@ -2,7 +2,7 @@
 # For copyright and license notices, see __manifest__.py file in module root
 # directory
 ##############################################################################
-from odoo import models, api, _
+from odoo import models, _
 
 
 class ProductProduct(models.Model):
