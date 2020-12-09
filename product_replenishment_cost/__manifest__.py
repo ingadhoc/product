b1391fe30718b800664f457dd718c93e20904894
Juan Jose Scarafia <jjs@adhoc.com.ar>
2020-12-09 16:35:25 -0300

    [IMP] product_replenishment_cost: tree view perf remove product m2m on tree view due to performance issues when rule is applied to many products Also remove items as it's not very useful

--- product_replenishment_cost/__manifest__.py
+++ product_replenishment_cost/__manifest__.py
@@ -19,7 +19,7 @@
 ##############################################################################
 {
     'name': 'Replenishment Cost',
-    'version': '13.0.1.2.0',
+    'version': '13.0.1.3.0',
     'author': "ADHOC SA, Camptocamp,GRAP,Odoo Community Association (OCA)",
     'license': 'AGPL-3',
     'category': 'Products',
