bcb07f7ad324cf2b7f01e4f7291f258e813b30c4
Juan Jose Scarafia <jjs@adhoc.com.ar>
2021-01-09 17:58:59 -0300

    [FIX] prices: use for save on readonly fields If the field is two times on the same view but one is readonly, the change is not saved, we use force_save to fix this

--- product_planned_price/__manifest__.py
+++ product_planned_price/__manifest__.py
@@ -19,7 +19,7 @@
 ##############################################################################
 {
     'name': 'Product Planned Price',
-    'version': '13.0.1.1.0',
+    'version': '13.0.1.2.0',
     'category': 'Product',
     'sequence': 14,
     'author': 'ADHOC SA,Odoo Community Association (OCA)',
