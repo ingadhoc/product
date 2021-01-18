35bf43e0de45b3a3402f82ea92df77b884c8b6d2
Juan Jose Scarafia <jjs@adhoc.com.ar>
2021-01-18 07:30:49 -0300

    [REF] fix authors

--- product_replenishment_cost/__manifest__.py
+++ product_replenishment_cost/__manifest__.py
@@ -1,26 +1,7 @@
-##############################################################################
-#
-#    Author:  Alexandre Fayolle
-#    Copyright 2012 Camptocamp SA
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
 {
     'name': 'Replenishment Cost',
     'version': '13.0.1.8.0',
-    'author': "ADHOC SA, Camptocamp,GRAP,Odoo Community Association (OCA)",
+    'author': "ADHOC SA, Odoo Community Association (OCA)",
     'license': 'AGPL-3',
     'category': 'Products',
     'depends': [
@@ -33,7 +14,6 @@
         # only for menu for cost rules
         'sale',
     ],
-    'website': 'http://www.camptocamp.com/',
     'data': [
         'security/product_replenishment_cost_security.xml',
         'data/ir_cron_data.xml',
