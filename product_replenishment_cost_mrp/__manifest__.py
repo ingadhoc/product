8b530e67eef1fa49edfa2b90ecb0c1fb41190268
Juan Jose Scarafia <jjs@adhoc.com.ar>
2021-01-18 07:30:49 -0300

    [REF] fix authors

--- product_replenishment_cost_mrp/__manifest__.py
+++ product_replenishment_cost_mrp/__manifest__.py
@@ -1,14 +1,13 @@
 {
     'name': 'Integration between Replenishment Cost and Manufacture',
     'version': '12.0.1.0.0',
-    'author': "ADHOC SA, Camptocamp, GRAP, Odoo Community Association (OCA)",
+    'author': "ADHOC SA, Odoo Community Association (OCA)",
     'license': 'AGPL-3',
     'category': 'Products',
     'depends': [
         'product_replenishment_cost',
         'mrp',
     ],
-    'website': 'http://www.camptocamp.com/',
     'data': [
         'views/mrp_bom_views.xml',
         'report/mrp_report_bom_structure.xml'
