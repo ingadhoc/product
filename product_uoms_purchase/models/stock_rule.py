f90b17db0591a1b193a6b8e69d1b7d3522143f7f
Juan Jose Scarafia <jjs@adhoc.com.ar>
2021-02-16 10:43:25 -0300

    [FIX] product_uoms_purchase

--- product_uoms_purchase/models/stock_rule.py
+++ product_uoms_purchase/models/stock_rule.py
@@ -8,8 +8,7 @@ from odoo import models
 class StockRule(models.Model):
     _inherit = 'stock.rule'
 
-    def _prepare_purchase_order_line(
-            self, product_id, product_qty, product_uom, values, po, supplier):
+    def _prepare_purchase_order_line(self, product_id, product_qty, product_uom, company_id, values, po):
 
         """ We modify this method to set the uom select for purchase in the
         UOMs of the product, by de onchange selected the correctly
@@ -18,7 +17,7 @@ class StockRule(models.Model):
         """
         res = super()._prepare_purchase_order_line(
             product_id=product_id, product_qty=product_qty,
-            product_uom=product_uom, values=values, po=po, supplier=supplier)
+            product_uom=product_uom, company_id=company_id, values=values, po=po)
 
         purchase_line = self.env['purchase.order.line'].new(res)
         purchase_line.onchange_product_id()
