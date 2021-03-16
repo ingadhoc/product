2a7b41f948c1c5afedfcf44af85a4c725fa1fec5
Juan Jose Scarafia <jjs@adhoc.com.ar>
2021-03-16 09:09:19 -0300

    [IMP] product_untranslated: add description and desc sale This two fields are used on website_sale search (splitting by world), so untranslating them would speed up search

--- product_untranslated/migrations/13.0.1.1.0/post-migration.py
+++ product_untranslated/migrations/13.0.1.1.0/post-migration.py
@@ -1,20 +1,14 @@
-from odoo import SUPERUSER_ID, api
-import logging
-_logger = logging.getLogger(__name__)
+from openupgradelib import openupgrade
 
 
-def post_init_hook(cr, registry):
-    """
-    Create a payment group for every existint payment
-    """
-    env = api.Environment(cr, SUPERUSER_ID, {})
+@openupgrade.migrate(use_env=True)
+def migrate(env, version):
     lang_read = env['res.lang'].search_read(['&', ('active', '=', True), ('code', '!=', 'en_US')], ['code'])
     if len(lang_read) != 1:
         # no language besides english or more than one language, no need to sync
         return True
     lang_code = lang_read[0]['code']
     models_fields = [
-        ('product.template', 'name'),
         ('product.template', 'description_sale'),
         ('product.template', 'description'),
     ]
@@ -23,7 +17,6 @@ def post_init_hook(cr, registry):
 
 
 def sync_field(env, lang_code, model_name, field_name):
-    _logger.info('Syncking translations for language %s for model %s, field %s' % (lang_code, model_name, field_name))
     translations = env['ir.translation'].search_read([
         ('name', '=', '%s,%s' % (model_name, field_name)),
         ('type', '=', 'model'),
