b847751a5eca4918286b1e40cf92fe30d170d48a
Nicolás Mac Rouillon <nmr@adhoc.com.ar>
2021-06-01 11:14:25 -0300

    [13.0] [FIX] price_security: Only restrict to write on customer invoices.

--- price_security/__manifest__.py
+++ price_security/__manifest__.py
@@ -19,7 +19,7 @@
 ##############################################################################
 {
     'name': 'Price Security',
-    'version': '13.0.1.1.0',
+    'version': '13.0.1.2.0',
     'category': 'Sales Management',
     'author': 'ADHOC SA, Odoo Community Association (OCA)',
     'website': 'http://www.adhoc.com.ar/',
