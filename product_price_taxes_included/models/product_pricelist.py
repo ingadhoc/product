583684253f858afe01a365b95eb70e26b5abeb67
nicolas <nmr@adhoc.com.ar>
2021-08-25 10:54:06 -0300

    [13.0] [IMP] product_price_taxes_included: Compatibility with product pack with fixed price .
    For the scenario of a price list items with fixed price for a product pack, the price of pack is not based in the components of them.
    Due of that situation we need to not skip the computation of the taxes for this case in the pack product, for the rest of the cases we consider that price is based in component prices with had included taxes# Por favor ingresa el mensaje del commit para tus cambios. Las

--- product_price_taxes_included/models/product_pricelist.py
+++ product_price_taxes_included/models/product_pricelist.py
@@ -19,13 +19,28 @@ class ProductPricelist(models.Model):
         res = super().get_products_price(
             products, quantities, partners, date=date, uom_id=uom_id)
         if self._context.get('taxes_included'):
-            company_id = (
-                self._context.get('company_id') or self.env.company.id)
+            company_id = (self._context.get('company_id')
+                          or self.env.company.id)
             for product in products:
                 # for compatibility with product_pack
-                if 'pack_ok' in product._fields and product.pack_ok and product.pack_component_price != 'ignored':
+                if 'pack_ok' in product._fields and self.check_for_product_pack_parent(product):
                     continue
                 res[product.id] = product.taxes_id.filtered(
                     lambda x: x.company_id.id == company_id).compute_all(
                         res[product.id], product=product.id)['total_included']
         return res
+
+    def check_for_product_pack_parent(self, product):
+        """
+        we use this method to handler several situation for product pack and pricelist 
+        We always return False (to continue with the calculation) except for some situation below
+        """
+        result = False
+        if product.pack_ok and product.pack_component_price != 'ignored':
+            if product._name == 'product.template' and product in self.item_ids.mapped('product_tmpl_id'):
+                result = False
+            elif product._name == 'product.product' and (product in self.item_ids.mapped('product_id') or product.product_tmpl_id in self.item_ids.mapped('product_tmpl_id')):
+                result = False
+            else:
+                result = True
+        return result
