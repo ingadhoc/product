a90903e9d1bd85a72b860f67735260cf8464981a
nicolas <nmr@adhoc.com.ar>
2021-09-21 14:35:37 -0300

    [13.0] [FIX] product_replenishment_cost_mrp: Fix the report of LMD when use other modules with mrp.
    We improve the way to change the replenishment cost to avoid override , because mrp_plm module use the methods for made the report values.

--- product_replenishment_cost_mrp/report/mrp_report_bom_structure.py
+++ product_replenishment_cost_mrp/report/mrp_report_bom_structure.py
@@ -5,53 +5,21 @@ from odoo.tools import float_round
 class ReportReplenishmentBomStructure(models.AbstractModel):
     _inherit = 'report.mrp.report_bom_structure'
 
-    def _get_bom(
-            self, bom_id=False, product_id=False, line_qty=False, line_id=False,
-            level=False):
+    def _get_bom(self, bom_id=False, product_id=False, line_qty=False, line_id=False, level=False):
+        """ Here we use the replenishment cost for the uom unit"""
+        res = super()._get_bom(bom_id=bom_id, product_id=product_id, line_qty=line_qty, line_id=line_id, level=level)
         bom = self.env['mrp.bom'].browse(bom_id)
         bom_quantity = line_qty
         if line_id:
             current_line = self.env['mrp.bom.line'].browse(int(line_id))
-            bom_quantity = current_line.product_uom_id._compute_quantity(
-                line_qty, bom.product_uom_id)
-        # Display bom components for current selected product variant
-        if product_id:
-            product = self.env['product.product'].browse(int(product_id))
-        else:
-            product = bom.product_id or bom.product_tmpl_id.product_variant_id
-        if product:
-            attachments = self.env['mrp.document'].search(
-                ['|', '&', ('res_model', '=', 'product.product'),
-                 ('res_id', '=', product.id),
-                 '&', ('res_model', '=', 'product.template'),
-                 ('res_id', '=', product.product_tmpl_id.id)])
-        else:
-            product = bom.product_tmpl_id
-            attachments = self.env['mrp.document'].search(
-                [('res_model', '=', 'product.template'), ('res_id', '=', product.id)])
-        operations = self._get_operation_line(bom.routing_id, float_round(
-            bom_quantity / bom.product_qty, precision_rounding=1, rounding_method='UP'), 0)
-        lines = {
-            'bom': bom, 'bom_qty': bom_quantity,
-            'bom_prod_name': product.display_name,
-            'currency': product.currency_id,
-            'product': product, 'code': bom and bom.display_name
-            or '', 'price': product.uom_id._compute_price(
-                product.replenishment_cost, bom.product_uom_id) * bom_quantity,
-            'total': sum([op['total'] for op in operations]),
-            'level': level or 0, 'operations': operations,
-            'operations_cost': sum([op['total'] for op in operations]),
-            'attachments': attachments,
-            'operations_time':
-            sum([op['duration_expected'] for op in operations])}
-        components, total = self._get_bom_lines(
-            bom, bom_quantity, product, line_id, level)
-        lines['components'] = components
-        lines['total'] += total
-        return lines
+            bom_quantity = current_line.product_uom_id._compute_quantity(line_qty, bom.product_uom_id) or 0
+        if res.get('product_id', False) and res['product_id']:
+            product = self.env['product.product'].browse(res['product_id'])
+            res['price'] = product.uom_id._compute_price(product.replenishment_cost, bom.product_uom_id) * bom_quantity
+        return res
 
     def _get_bom_lines(self, bom, bom_quantity, product, line_id, level):
-        components = []
+        components, total = super()._get_bom_lines(bom, bom_quantity, product, line_id, level)
         total = 0
         for line in bom.bom_line_ids:
             line_quantity = (
@@ -70,27 +38,11 @@ class ReportReplenishmentBomStructure(models.AbstractModel):
             else:
                 sub_total = price
             sub_total = line.product_id.currency_id.round(sub_total)
-            components.append({
-                'prod_id': line.product_id.id,
-                'prod_name': line.product_id.display_name,
-                'code': line.child_bom_id and line.child_bom_id.display_name or '',
-                'prod_qty': line_quantity,
-                'prod_uom': line.product_uom_id.name,
-                'prod_cost': price,
-                'parent_id': bom.id,
-                'line_id': line.id,
-                'level': level or 0,
-                'total': sub_total,
-                'child_bom': line.child_bom_id.id,
-                'phantom_bom': line.child_bom_id and line.child_bom_id.type == 'phantom' or False,
-                'attachments': self.env['mrp.document'].search(['|', '&',
-                                                                ('res_model', '=', 'product.product'),
-                                                                ('res_id', '=', line.product_id.id),
-                                                                '&',
-                                                                ('res_model', '=', 'product.template'),
-                                                                ('res_id', '=', line.product_id.product_tmpl_id.id)]),
-
-            })
+            for comp in components:
+                if comp['line_id'] == line.id:
+                    comp['prod_cost'] = price
+                    comp['total'] = sub_total
+                    break
             total += sub_total
         return components, total
 
