d7fc3ae5e7ad122741e7a315fb5b22bdfb340bba
Pablo Montenegro <pam@adhoc.com.ar>
2021-12-10 19:30:38 +0000

    [MIG] product_pricelist: Migration to 15.0
    closes ingadhoc/product#419
    
    Signed-off-by: Juan José Scarafía <jjs@adhoc.com.ar>

--- product_pricelist/__manifest__.py
+++ product_pricelist/__manifest__.py
@@ -19,7 +19,7 @@
 ##############################################################################
 {
     "name": "Pricelists information on Products",
-    'version': '13.0.1.1.0',
+    'version': "15.0.1.0.0",
     'category': 'Sales & Purchases',
     'sequence': 14,
     'author': 'ADHOC SA',
@@ -35,7 +35,7 @@
     ],
     'demo': [
     ],
-    'installable': False,
+    'installable': True,
     'auto_install': False,
     'application': False,
 }
