4e29269503fd1c4ca60c328174248493366c243d
Pablo Paez <pp@adhoc.com.ar>
2021-12-29 15:29:08 -0300

    [MIG] product_price_taxes_included: Migration to 15.0

--- product_price_taxes_included/__manifest__.py
+++ product_price_taxes_included/__manifest__.py
@@ -19,7 +19,7 @@
 ##############################################################################
 {
     'name': 'Product Price Taxes Included or Not',
-    'version': '13.0.1.1.0',
+    'version': "15.0.1.0.0",
     'category': 'Product',
     'sequence': 14,
     'summary': '',
@@ -39,7 +39,7 @@
     ],
     'demo': [
     ],
-    'installable': False,
+    'installable': True,
     'auto_install': False,
     'application': False,
 }
