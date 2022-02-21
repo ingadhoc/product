1899d4f23c08b6c8579b28aab455f628e1250606
Pablo Montenegro <pam@adhoc.com.ar>
2022-02-21 12:04:34 +0000

    [MIG] price_security_planned_price: Migration to 15.0
    closes ingadhoc/product#443
    
    Signed-off-by: Nicolas Mac Rouillon <nmr@adhoc.com.ar>

--- price_security_planned_price/__manifest__.py
+++ price_security_planned_price/__manifest__.py
@@ -19,7 +19,7 @@
 ##############################################################################
 {
     'name': 'Price Security with planned price',
-    'version': '13.0.1.1.0',
+    'version': "15.0.1.0.0",
     'category': 'Sales Management',
     'author': 'ADHOC SA',
     'website': 'http://www.adhoc.com.ar/',
@@ -31,6 +31,6 @@
     'data': [
         'views/product_template_views.xml',
     ],
-    'installable': False,
+    'installable': True,
     'auto_install': True,
 }
