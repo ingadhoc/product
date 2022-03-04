82abc3ebf294b82ea3aed93c217e22791a43f567
nicolas <nmr@adhoc.com.ar>
2022-03-04 13:18:55 -0300

    [MIG] product_uoms_purchase: Migration to 15.0

--- product_uoms_purchase/models/__init__.py
+++ product_uoms_purchase/models/__init__.py
@@ -4,5 +4,6 @@
 ##############################################################################
 from . import product_uoms
 from . import product_product
+from . import product_template
 from . import purchase_order_line
 from . import stock_rule
