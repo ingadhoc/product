0f0132da9a2d36f0bdc0899f43248874de1dd40b
nicolas <nmr@adhoc.com.ar>
2022-03-08 16:21:20 -0300

    [MIG] product_uoms_sale: Migration to 15.0

--- product_uoms_sale/__manifest__.py
+++ product_uoms_sale/__manifest__.py
@@ -19,7 +19,7 @@
 ##############################################################################
 {
     'name': 'Product UOMS Sale',
-    'version': '13.0.1.0.0',
+    'version': "15.0.1.0.0",
     'category': 'base.module_category_knowledge_management',
     'author': 'ADHOC SA',
     'website': 'www.adhoc.com.ar',
@@ -35,7 +35,7 @@
         'views/product_template_views.xml',
         'security/ir.model.access.csv',
     ],
-    'installable': False,
+    'installable': True,
     'auto_install': True,
     'application': False,
 }
