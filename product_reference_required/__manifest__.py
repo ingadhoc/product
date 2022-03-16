06e5dec49a1e788c6caf84c41bed9cfbe60f5ea9
Joel Zilli <joz@adhoc.com.ar>
2022-03-16 09:11:15 -0300

    [MIG] product_reference_required: Migration to 15.0

--- product_reference_required/__manifest__.py
+++ product_reference_required/__manifest__.py
@@ -19,7 +19,7 @@
 ##############################################################################
 {
     'name': 'Product Reference Requiered',
-    'version': '13.0.1.0.0',
+    'version': "15.0.1.0.0",
     'category': 'Product',
     'sequence': 14,
     'summary': '',
@@ -37,7 +37,7 @@
     'demo': [
         'demo/ir_sequence_demo.xml',
     ],
-    'installable': False,
+    'installable': True,
     'auto_install': False,
     'application': False,
     'pre_init_hook': 'set_default_code',
