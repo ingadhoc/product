27e1eacd8473cd676dc169c574c93cdc5cc6a600
nicolas <nmr@adhoc.com.ar>
2022-05-13 13:45:58 -0300

    [MIG] product_catalog_aeroo_report_public_categ: Migration to 15.0

--- product_catalog_aeroo_report_public_categ/models/product_public_category.py
+++ product_catalog_aeroo_report_public_categ/models/product_public_category.py
@@ -2,32 +2,19 @@
 # For copyright and license notices, see __manifest__.py file in module root
 # directory
 ##############################################################################
-from odoo import models, fields, api
-import logging
-_logger = logging.getLogger(__name__)
+from odoo import models, fields
 
 
 class ProductPublicCategory(models.Model):
     _inherit = "product.public.category"
 
-    _parent_name = "parent_id"
-    _parent_store = True
     _parent_order = 'sequence, name'
+
     parent_id = fields.Many2one(
         ondelete='restrict',
-    )
-    parent_path = fields.Char(
-        'Left',
-        index=True,
     )
     complete_name = fields.Char(
         'Complete Name',
         related='display_name',
         readonly=True,
     )
-
-    @api.constrains('sequence')
-    def update_parents(self):
-        # parent recompute is only trigger if parent_id field change
-        # we force recompute also if we change sequence
-        self._parent_store_compute()
