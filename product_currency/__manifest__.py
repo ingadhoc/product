9b9caf0d98a89a979b6387f8879dc1abcd32571e
matiasperalta1 <matias_peralta1@hotmail.com>
2022-10-28 18:30:44 +0000

    [MIG] product_currency: Migration to 16.0
    closes ingadhoc/product#466
    
    Signed-off-by: Juan José Scarafía <jjs@adhoc.com.ar>

--- product_currency/__manifest__.py
+++ product_currency/__manifest__.py
@@ -19,7 +19,7 @@
 ##############################################################################
 {
     'name': 'Product Currency',
-    'version': "15.0.1.0.0",
+    'version': "16.0.1.0.0",
     'category': 'Products',
     'sequence': 14,
     'summary': '',
@@ -37,7 +37,7 @@
     ],
     'demo': [
     ],
-    'installable': False,
+    'installable': True,
     'auto_install': False,
     'application': False,
 }
