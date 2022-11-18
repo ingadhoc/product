6b15d6040c993d50347a297d8d99efef45eb8b9c
Virginia Bonservizi <vib@adhoc.com.ar>
2022-11-18 16:44:51 +0000

    [ADD] product_expiry_ux: create module.
    closes ingadhoc/product#504
    
    X-original-commit: ecc7acc9aa002b50fb81b0dff319e98a357bf664
    Signed-off-by: augusto-weiss <awe@adhoc.com.ar>
    Signed-off-by: Virginia Bonservizi <vib@adhoc.com.ar>

---
+++ product_expiry_ux/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'Products Expiration Date UX',
+    'version': '15.0.1.0.0',
+    'category': 'Inventory/Inventory',
+    'author': 'ADHOC SA',
+    'website': 'www.adhoc.com.ar',
+    'depends': ['product_expiry'],
+    'data': ['views/production_lot_views.xml'],
+    'demo': [],
+    'license': 'AGPL-3',
+    'installable': False,
+    'auto_install': True,
+    'application': False,
+}
