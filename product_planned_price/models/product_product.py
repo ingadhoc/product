684b0131ed25294a5350f189f8ea4e88302090e9
augusto-weiss <awe@adhoc.com.ar>
2022-12-01 13:59:41 +0000

    [MIG] product_planned_price: Migration to 16.0
    closes ingadhoc/product#472
    
    Signed-off-by: Bruno Zanotti <bz@adhoc.com.ar>

--- product_planned_price/models/product_product.py
+++ product_planned_price/models/product_product.py
@@ -9,7 +9,7 @@ class ProductProduct(models.Model):
     _inherit = "product.product"
 
     def price_compute(
-            self, price_type, uom=False, currency=False, company=False):
+            self, price_type, uom=False, currency=False, company=False, date=False):
         """
         We do this so that if someone wants to force the calculated price
         be based on the planned, you can do it by sending use_planned_price
@@ -18,4 +18,4 @@ class ProductProduct(models.Model):
         if self._context.get(
                 'use_planned_price') and price_type == 'list_price':
             price_type = 'computed_list_price'
-        return super().price_compute(price_type, uom=uom, currency=currency, company=company)
+        return super().price_compute(price_type, uom=uom, currency=currency, company=company, date=date)
--- product_planned_price/models/product_template.py
+++ product_planned_price/models/product_template.py
@@ -136,7 +136,7 @@ class ProductTemplate(models.Model):
             })
 
     def price_compute(
-            self, price_type, uom=False, currency=False, company=False):
+            self, price_type, uom=False, currency=False, company=False, date=False):
         """
         We do this so that if someone wants to force the calculated price
         be based on the planned, you can do it by sending use_planned_price
@@ -145,4 +145,4 @@ class ProductTemplate(models.Model):
         if self._context.get(
                 'use_planned_price') and price_type == 'list_price':
             price_type = 'computed_list_price'
-        return super().price_compute(price_type, uom=uom, currency=currency, company=company)
+        return super().price_compute(price_type, uom=uom, currency=currency, company=company, date=date)
