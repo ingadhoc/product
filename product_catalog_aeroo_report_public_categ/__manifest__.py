3cd43c5de06ec60c1256d43e8cb7fd22074c5252
Virginia Bonservizi <vib@adhoc.com.ar>
2022-12-12 18:50:55 +0000

    [MIG] product_catalog_aeroo_report_public_categ: Migration to 16.0
    closes ingadhoc/product#476
    
    Signed-off-by: Nicolas Mac Rouillon <nmr@adhoc.com.ar>

--- product_catalog_aeroo_report_public_categ/__manifest__.py
+++ product_catalog_aeroo_report_public_categ/__manifest__.py
@@ -19,7 +19,7 @@
 ##############################################################################
 {
     'name': 'Product Catalog Aeroo Report with public Category',
-    'version': "15.0.1.0.0",
+    'version': "16.0.1.0.0",
     'category': 'Aeroo Reporting',
     'sequence': 14,
     'summary': '',
@@ -37,7 +37,7 @@
     ],
     'demo': [
     ],
-    'installable': False,
+    'installable': True,
     'auto_install': True,
     'application': False,
 }
