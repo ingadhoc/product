a938d9b747f0a6d5704f6bf86388b948d06c1eb6
José Antonio Keh <jok@adhoc.com.ar>
2022-12-15 14:47:45 -0300

    [MIG] product_template_tree_first: Migration to 16.0

--- product_template_tree_first/__manifest__.py
+++ product_template_tree_first/__manifest__.py
@@ -19,7 +19,7 @@
 ##############################################################################
 {
     'name': 'Product Template Tree First',
-    'version': "15.0.1.0.0",
+    'version': "16.0.1.0.0",
     'category': 'Sales Management',
     'sequence': 14,
     'summary': '',
@@ -31,7 +31,7 @@
     'depends': [
         'product',
     ],
-    'installable': False,
+    'installable': True,
     'auto_install': False,
     'application': False,
 }
