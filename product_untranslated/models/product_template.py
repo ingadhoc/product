145aba9ea35c797ff82f6b128ce068c884751684
Juan Jose Scarafia <jjs@adhoc.com.ar>
2022-12-15 16:00:35 -0300

    [REM] product_untranslated deprecated

--- product_untranslated/models/product_template.py
+++
@@ -1,9 +0,0 @@
-from odoo import models, fields
-
-
-class ProductTemplate(models.Model):
-    _inherit = "product.template"
-
-    name = fields.Char(translate=False)
-    description_sale = fields.Text(translate=False)
-    description = fields.Text(translate=False)
