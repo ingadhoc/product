d37e85a609f7ba647cb3d35fa8c4ba9df81547a4
Virginia Bonservizi <vib@adhoc.com.ar>
2023-01-06 12:38:42 -0300

    [MIG] price_security_planned_price: Migration to 16.0

--- price_security_planned_price/__init__.py
+++ price_security_planned_price/__init__.py
@@ -2,3 +2,4 @@
 # For copyright and license notices, see __manifest__.py file in module root
 # directory
 ##############################################################################
+from . import models
