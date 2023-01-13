2ace0f0cb1a3dbeef87969787a7414f22bd69720
juancarreras97 <jc@adhoc.com.ar>
2023-01-13 14:31:49 -0300

    [IMP]customization of zpl report

--- product_ux/__manifest__.py
+++ product_ux/__manifest__.py
@@ -19,7 +19,7 @@
 ##############################################################################
 {
     'name': 'Product UX',
-    'version': "13.0.1.3.0",
+    'version': "13.0.1.4.0",
     'category': 'Products',
     'sequence': 14,
     'summary': '',
@@ -37,6 +37,7 @@
         'views/product_pricelist_item_view.xml',
         'views/product_pricelist_view.xml',
         'views/uom_uom_views.xml',
+        'report/picking_templates.xml',
     ],
     'demo': [
     ],
