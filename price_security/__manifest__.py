8cb23bf53d9f94891bbc2790fad63dd6dcd1feba
Virginia Bonservizi <vib@adhoc.com.ar>
2023-02-13 12:41:13 -0300

    [FIX] price_security: make lst_pice readonly on product.product

--- price_security/__manifest__.py
+++ price_security/__manifest__.py
@@ -19,7 +19,7 @@
 ##############################################################################
 {
     'name': 'Price Security',
-    'version': "15.0.1.3.0",
+    'version': "15.0.1.4.0",
     'category': 'Sales Management',
     'author': 'ADHOC SA, Odoo Community Association (OCA)',
     'website': 'http://www.adhoc.com.ar/',
