5fb05128944a4397c426730cf4a5952fb664f62b
augusto-weiss <awe@adhoc.com.ar>
2023-03-23 12:48:27 +0000

    [IMP] product_replenishment_cost: improve the update of last_date_price_updated
    X-original-commit: 32cb6338312d1a3ca1dd188aa5dc5e763582c191

--- product_replenishment_cost/__manifest__.py
+++ product_replenishment_cost/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Replenishment Cost',
-    'version': "16.0.1.2.0",
+    'version': "16.0.1.3.0",
     'author': "ADHOC SA, Odoo Community Association (OCA)",
     'license': 'AGPL-3',
     'category': 'Products',
