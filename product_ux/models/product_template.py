454c283439354a05a1beb69d298b6a3af4a1480b
Bruno Zanotti <bz@adhoc.com.ar>
2023-08-15 09:27:53 -0300

    [TMP FIX] compute odumbo prices

--- product_ux/models/product_template.py
+++ product_ux/models/product_template.py
@@ -15,9 +15,18 @@ class ProductTemplate(models.Model):
     )
     warranty = fields.Float()
     pricelist_price = fields.Float(compute='_compute_product_pricelist_price', digits='Product Price')
+    odumbo_price = fields.Float(compute='_compute_product_odumbo_price', digits='Product Price')
     pricelist_id = fields.Many2one('product.pricelist', store=False,)
 
     @api.depends_context('pricelist', 'quantity', 'uom', 'date', 'no_variant_attributes_price_extra')
     def _compute_product_pricelist_price(self):
         for product in self:
             product.pricelist_price = product._get_contextual_price()
+
+    @api.depends_context('pricelist', 'quantity', 'uom', 'date', 'no_variant_attributes_price_extra')
+    def _compute_product_odumbo_price(self):
+        for record in self:
+            company_id = self._context.get('company_id', self.env.company.id)
+            price = record._get_contextual_price()
+            res = record.taxes_id.filtered(lambda x: x.company_id.id == company_id).compute_all(price, product=record)
+            record.odumbo_price = res['total_included']
