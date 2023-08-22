05a77b21c5a847648fa62eb96efb94575d408969
jok-adhoc <jok@adhoc.com.ar>
2023-08-22 16:33:41 +0000

    Cherry pick of aa7eeda4a0d0e66f0a489755d19405eb41ba2f4a failed
    stdout:
    CONFLICT (add/add): Merge conflict in price_security_replenishment_cost/__manifest__.py
    Auto-merging price_security_replenishment_cost/__manifest__.py
    
    stderr:
    23:13:56.429352 git.c:444               trace: built-in: git cherry-pick aa7eeda4a0d0e66f0a489755d19405eb41ba2f4a
    error: could not apply aa7eeda4... [IMP]price_security_replenishment_cost: new module
    hint: after resolving the conflicts, mark the corrected paths
    hint: with 'git add <paths>' or 'git rm <paths>'
    hint: and commit the result with 'git commit'
    ----------
    status:

--- price_security_sale_margin/__manifest__.py
+++ price_security_sale_margin/__manifest__.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 ##############################################################################
 #
 #    Copyright (C) 2015  ADHOC SA  (http://www.adhoc.com.ar)
@@ -31,3 +32,39 @@
     'installable': False,
     'auto_install': True,
 }
+||||||| parent of d5e1e8f4 (temp)
+=======
+##############################################################################
+#
+#    Copyright (C) 2015  ADHOC SA  (http://www.adhoc.com.ar)
+#    All Rights Reserved.
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+{
+    'name': 'Price Security with sale margin',
+    'version': "16.0.1.0.0",
+    'category': 'Sales Management',
+    'author': 'ADHOC SA',
+    'website': 'http://www.adhoc.com.ar/',
+    'license': 'AGPL-3',
+    'depends': [
+        'price_security',
+        'sale_margin',
+    ],
+    'installable': True,
+    'auto_install': True,
+}
+>>>>>>> d5e1e8f4 (temp)
