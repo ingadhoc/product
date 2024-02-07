2212845e9b3aba30887d0f0e963f744fe73a477c
Joaquin vega <jov@adhoc.com.ar>
2024-02-07 14:47:44 -0300

    Último commit de inducción jov

--- estate_account/models/estate_property.py
+++ estate_account/models/estate_property.py
@@ -1,18 +1,35 @@
-from odoo import fields, models, api
+from odoo import fields, models, api, Command
 
 class EstatePropertyInherited(models.Model):
     _inherit = 'estate.property'
 
-    prubeaherencia = fields.Integer('PRUEBAHERENCIA')
+   
     
 
         
     def action_property_sold(self):
+
+        res = super().action_property_sold()
+
         for record in self:
             record.env["account.move"].create(
                 {
                     "partner_id" : record.buyer_id.id,
-                    "move_type" : "out_invoice"
+                    "move_type" : "out_invoice",
+                    "invoice_line_ids" : [
+                        Command.create({
+                            "name" : "Administrative fee",
+                            "quantity" : "1",
+                            "price_unit" : "100"
+                            }),
+                        
+                        Command.create({
+                            "name" : "Property",
+                            "quantity" : "1",
+                            "price_unit" : record.selling_price / 100 * 6,
+                            })
+                    ]
+
                 }
 
 
