ab9eac124386fe94c0d6cd734002e46cd829e872
mav-adhoc <mav@adhoc.com.ar>
2024-03-27 17:30:34 +0000

    [ADD] product_expiry_ux: expiration_date in lot
    closes ingadhoc/product#609
    
    Related: ingadhoc/stock#465
    Signed-off-by: Juan Carreras <jc@adhoc.com.ar>

---
+++ product_expiry_ux/models/stock_move_line.py
@@ -0,0 +1,28 @@
+# For copyright and license notices, see __manifest__.py file in module root
+# directory
+##############################################################################
+import datetime
+from odoo import models, api, fields, _
+from odoo.exceptions import ValidationError
+from odoo.tools import float_is_zero
+
+class StockMoveLine(models.Model):
+
+    _inherit = 'stock.move.line'
+
+    @api.onchange('expiration_date')
+    def onchange_expiration_date(self):
+        if self.lot_id and self.product_id:
+            lot = self.env['stock.lot'].search([
+                ('name', '=', self.lot_id.name),
+                ('product_id', '=', self.product_id.id),
+            ])
+            prod = lot.product_id.product_tmpl_id
+            lot.write({
+                'expiration_date': self.expiration_date,
+            })
+            lot.write({
+                'removal_date': lot.expiration_date - datetime.timedelta(days=prod.removal_time),
+                'alert_date': lot.expiration_date - datetime.timedelta(days=prod.alert_time),
+                'use_date': lot.expiration_date - datetime.timedelta(days=prod.use_time),
+            })
