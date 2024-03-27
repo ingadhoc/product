0f05cdbea966f164c315074f526e62ae371f96e0
mav-adhoc <mav@adhoc.com.ar>
2024-03-27 14:30:34 -0300

    [ADD] product_expiry_ux: expiration_date in lot

--- product_expiry_ux/__manifest__.py
+++ product_expiry_ux/__manifest__.py
@@ -1,11 +1,17 @@
 {
     'name': 'Products Expiration Date UX',
-    'version': '16.0.1.0.0',
+    'version': '16.0.1.1.0',
     'category': 'Inventory/Inventory',
     'author': 'ADHOC SA',
     'website': 'www.adhoc.com.ar',
-    'depends': ['product_expiry'],
-    'data': ['views/production_lot_views.xml'],
+    'depends': [
+        'product_expiry',
+        'stock_ux',
+    ],
+    'data': [
+        'views/production_lot_views.xml',
+        'views/stock_move_line_views.xml',
+    ],
     'demo': [],
     'license': 'AGPL-3',
     'installable': True,
