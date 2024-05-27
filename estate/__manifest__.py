ab822a441203ebf263b30691167114dc4a8b0e69
Daniel Libonati <dal@adhoc.com.ar>
2024-05-27 11:48:09 -0300

    [ADD] estate module

---
+++ estate/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    'name': 'Real Estate',
+    'version': '1.0',
+    'license': 'LGPL-3',
+    'category': 'Sales/Real Estate',
+    'sequence': 15,
+    'summary': 'Real Estate Management',
+    'description': "",
+    'depends': ['base'],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/estate_property_views.xml'
+    ],
+    'demo': [],
+    'css': [],
+    'installable': True,
+    'application': True,
+    'auto_install': False
+}
