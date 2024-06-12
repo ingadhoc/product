18544b1ca61172d6b1560843431450ed218dec8a
mav-adhoc <mav@adhoc.com.ar>
2024-06-12 14:23:03 -0300

    [MIG] product_replenishment_cost_stock: Migration to 17.0

--- product_replenishment_cost_stock/__manifest__.py
+++ product_replenishment_cost_stock/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Integration between Replenishment Cost and purchase stock',
-    'version': "16.0.1.0.0",
+    'version': "17.0.1.0.0",
     'author': "ADHOC SA",
     'license': 'AGPL-3',
     'category': 'Products',
@@ -10,6 +10,6 @@
     ],
     'data': [
     ],
-    'installable': False,
+    'installable': True,
     'auto_install': True,
 }
