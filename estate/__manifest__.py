c5bdc1931c2933d22e9e103074eee0b810b4d731
Felipe Garcia Suez <feg@adhoc.com.ar>
2024-07-01 12:13:43 -0300

    [ADD] estate module

---
+++ estate/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': "Real Estate",
+    'depends': ['base'],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/estate_property_views.xml',
+        'views/estate_property_type_views.xml',
+        'views/estate_property_tag_views.xml',
+        'views/estate_property_offer_views.xml',
+        'views/estate_property_menus.xml'
+    ]
+}
