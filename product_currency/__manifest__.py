1b414b49f1ecde1b5adb9e982cc3b462780c8e4d
docker-odoo <docker-odoo@example.com>
2024-10-10 18:45:52 +0000

    [MIG] product_currency: Migration to 18.0

--- product_currency/__manifest__.py
+++ product_currency/__manifest__.py
@@ -19,7 +19,7 @@
 ##############################################################################
 {
     'name': 'Product Currency',
-    'version': "17.0.1.0.0",
+    'version': "18.0.1.0.0",
     'category': 'Products',
     'sequence': 14,
     'summary': '',
@@ -38,7 +38,7 @@
     'demo': [
         'demo/product_product_demo.xml',
     ],
-    'installable': False,
+    'installable': True,
     'auto_install': False,
     'application': False,
 }
