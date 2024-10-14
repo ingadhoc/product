4496cdb3e4c7b4522f53fb561117af0249dc5378
docker-odoo <docker-odoo@example.com>
2024-10-14 12:40:10 +0000

    [MIG] product_template_tree_first: Migration to 18.0
    closes ingadhoc/product#672
    
    Signed-off-by: Juan Carreras <jc@adhoc.com.ar>

--- product_template_tree_first/models/ir_actions_act_window.py
+++ product_template_tree_first/models/ir_actions_act_window.py
@@ -7,20 +7,20 @@ class IrActionsActWindow(models.Model):
 
     @api.depends('view_ids.view_mode', 'view_mode', 'view_id.type')
     def _compute_views(self):
-        """ Force the tree view to always be the first one, no matter what is
+        """ Force the list view to always be the first one, no matter what is
         configured in the action
         """
         super()._compute_views()
         product_template = self.sudo().filtered(
             lambda x: x.res_model == 'product.template' and
-            'tree' in x.view_mode)
+            'list' in x.view_mode)
         for act in product_template.with_context(from_config=True):
             modes = act.view_mode.split(',')
-            modes.remove('tree')
-            modes = ['tree'] + modes
+            modes.remove('list')
+            modes = ['list'] + modes
             act.view_mode = ','.join(modes)
             for item in act.views:
-                if item[1] == 'tree':
+                if item[1] == 'list':
                     list_view = item
                     act.views.remove(item)
                     act.views = [list_view] + act.views
