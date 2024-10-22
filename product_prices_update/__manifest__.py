c12a1b7ea7be7cf9d6c2c0e9021f25901771d974
docker-odoo <docker-odoo@example.com>
2024-10-22 20:36:39 +0000

    [MIG] product_prices_update: Migration to 18.0
    closes ingadhoc/product#684
    
    Signed-off-by: lav-adhoc <lav@adhoc.com.ar>

--- product_prices_update/__manifest__.py
+++ product_prices_update/__manifest__.py
@@ -19,7 +19,7 @@
 ##############################################################################
 {
     'name': 'Product Prices Update',
-    'version': "17.0.1.0.0",
+    'version': "18.0.1.0.0",
     'category': 'Product',
     'sequence': 14,
     'summary': '',
@@ -37,7 +37,7 @@
     ],
     'demo': [
     ],
-    'installable': False,
+    'installable': True,
     'auto_install': False,
     'application': False,
 }
