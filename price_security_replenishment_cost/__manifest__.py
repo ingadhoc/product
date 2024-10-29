1c8dcfabaada6870d1533d07ddbc97104fc97ae1
docker-odoo <docker-odoo@example.com>
2024-10-29 14:34:34 +0000

    [MIG] price_security_replenishment_cost: Migration to 18.0

--- price_security_replenishment_cost/__manifest__.py
+++ price_security_replenishment_cost/__manifest__.py
@@ -19,7 +19,7 @@
 ##############################################################################
 {
     'name': 'Price Security with replenishment cost',
-    'version': "17.0.1.0.0",
+    'version': "18.0.1.0.0",
     'category': 'Sales Management',
     'author': 'ADHOC SA',
     'website': 'http://www.adhoc.com.ar/',
@@ -28,6 +28,6 @@
         'price_security',
         'product_replenishment_cost',
     ],
-    'installable': False,
+    'installable': True,
     'auto_install': True,
 }
