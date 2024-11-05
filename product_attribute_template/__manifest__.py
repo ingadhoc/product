9f533f2185ff7f0b361d254376c80caecb25f714
docker-odoo <docker-odoo@example.com>
2024-11-05 17:38:34 +0000

    [MIG] product_attribute_template: Migration to 18.0

--- product_attribute_template/__manifest__.py
+++ product_attribute_template/__manifest__.py
@@ -1,13 +1,12 @@
 {
     'name': 'Product Attribute Template',
-    'version': "17.0.1.0.0",
+    'version': "18.0.1.0.0",
     'category': 'Sales Management',
     'author': 'ADHOC SA, Odoo Community Association (OCA)',
     'website': 'http://www.adhoc.com.ar/',
     'license': 'AGPL-3',
     'depends': [
         'product',
-        'web_widget_x2many_2d_matrix',
         'sale',
     ],
     'data': [
@@ -15,5 +14,5 @@
         'views/product_attribute_template_views.xml',
         'views/product_template_views.xml',
     ],
-    'installable': False,
+    'installable': True,
 }
