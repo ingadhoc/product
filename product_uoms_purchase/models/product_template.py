ee9e83003b6e39c4e976d4a2ce52985f39e70345
Juan Ignacio Carreras <jc@adhoc.com.ar>
2024-11-21 16:03:53 -0300

    [FIX]product_uoms_purchase:change in compute

--- product_uoms_purchase/models/product_template.py
+++ product_uoms_purchase/models/product_template.py
@@ -2,23 +2,16 @@
 # For copyright and license notices, see __manifest__.py file in module root
 # directory
 ##############################################################################
-from odoo import models, fields, api
+from odoo import models, api
 
 
 class ProductTemplate(models.Model):
     _inherit = 'product.template'
 
-    uom_po_id = fields.Many2one(
-        store=True,
-        compute="_compute_uom_po_id",
-        readonly=False,
-    )
-
     @api.depends('uom_ids.sequence', 'uom_ids.purchase_ok')
     def _compute_uom_po_id(self):
+        super()._compute_uom_po_id()
         for rec in self.filtered('uom_ids'):
             purchase_uoms = rec.uom_ids.filtered('purchase_ok').sorted().mapped('uom_id')
             if purchase_uoms:
                 rec.uom_po_id = purchase_uoms[0]
-            else:
-                rec.uom_po_id = rec.uom_id
