e5b20de4748a1d3c32d560532d10231d49e070ee
docker-odoo <docker-odoo@example.com>
2024-11-26 19:15:06 +0000

    [MIG] product_brand_report: Migration to 18.0

--- product_brand_report/__manifest__.py
+++ product_brand_report/__manifest__.py
@@ -19,7 +19,7 @@
 ##############################################################################
 {
     'name': 'Product Brand Report',
-    'version': "17.0.1.0.0",
+    'version': "18.0.1.0.0",
     'category': 'Product',
     'author': 'ADHOC SA',
     'website': 'www.adhoc.com.ar',
@@ -34,7 +34,7 @@
         'reports/report_invoice_document.xml',
         'views/res_config_settings_view.xml'
     ],
-    'installable': False,
+    'installable': True,
     'auto_install': False,
     'application': False,
 }
