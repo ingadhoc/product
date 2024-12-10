6680e974ff754ad584db112e771c15e23dd42d7b
Matias Peralta <matias_peralta1@hotmail.com>
2024-12-10 16:10:57 +0000

    [ADD] product_catalog_tree: fields in search view
    closes ingadhoc/product#711
    
    X-original-commit: a0e2a2dbedeac487f675035017db09a71a54ec75
    Signed-off-by: lav-adhoc <lav@adhoc.com.ar>
    Signed-off-by: matiasperalta1 <mnp@adhoc.com.ar>

--- product_catalog_tree/__manifest__.py
+++ product_catalog_tree/__manifest__.py
@@ -19,7 +19,7 @@
 ##############################################################################
 {
     'name': 'Product Catalog Tree',
-    'version': "18.0.1.0.0",
+    'version': "18.0.1.1.0",
     'category': 'Products',
     'sequence': 14,
     'summary': '',
