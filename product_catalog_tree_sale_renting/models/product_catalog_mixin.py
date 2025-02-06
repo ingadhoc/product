10d1574e67a2d4c01f90962e1c8d19c5bbde3f15
Matias Peralta <matias_peralta1@hotmail.com>
2025-02-06 13:01:15 -0300

    [ADD] product_catalog_tree_sale_renting: adjust behavior of catalog into rentals

---
+++ product_catalog_tree_sale_renting/models/product_catalog_mixin.py
@@ -0,0 +1,13 @@
+from odoo import _, models
+
+
+class ProductCatalogMixin(models.AbstractModel):
+    _inherit = 'product.catalog.mixin'
+    _description = 'Product Catalog Mixin'
+
+    def action_add_from_catalog(self):
+        action = super().action_add_from_catalog()
+        if self.is_rental_order:
+            tree_view_id = self.env.ref('product_catalog_tree.product_view_tree_catalog').id
+            action['views'] = [view for view in action['views'] if view != (tree_view_id, 'tree')]
+        return action
