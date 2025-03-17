2963aaab0f4f67163265528a6c6522a2cf103f22
Franco Leyes <lef@adhoc.com.ar>
2025-03-17 18:53:54 +0000

    [FIX] price_security: restrict global discount and fixed amount
    closes ingadhoc/product#732
    
    Signed-off-by: matiasperalta1 <mnp@adhoc.com.ar>

--- price_security/__manifest__.py
+++ price_security/__manifest__.py
@@ -19,7 +19,7 @@
 ##############################################################################
 {
     'name': 'Price Security',
-    'version': "17.0.1.3.0",
+    'version': "17.0.1.4.0",
     'category': 'Sales Management',
     'author': 'ADHOC SA, Odoo Community Association (OCA)',
     'website': 'http://www.adhoc.com.ar/',
@@ -32,6 +32,7 @@
     'data': [
         'security/price_security_security.xml',
         'security/ir.model.access.csv',
+        'wizard/sale_order_discount_views.xml',
         'views/account_move_views.xml',
         'views/account_payment_term_views.xml',
         'views/product_template_views.xml',
