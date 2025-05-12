f8a35432961f47aee7db763333abcaec9899f977
Lara <lav@adhoc.com.ar>
2025-05-12 09:33:43 -0300

    [FIX] product_catalog_tree: edit quantity in tree views

--- product_catalog_tree/models/product_product.py
+++ product_catalog_tree/models/product_product.py
@@ -2,6 +2,8 @@
 # For copyright and license notices, see __manifest__.py file in module root
 # directory
 ##############################################################################
+import json
+
 from lxml import etree
 from odoo import api, fields, models
 
@@ -12,7 +14,6 @@ class ProductProduct(models.Model):
     product_catalog_qty = fields.Float(
         string="Quantity",
         compute="_compute_catalog_values",
-        inverse="_inverse_catalog_values",
     )
     product_catalog_price = fields.Float(
         string="Order Price",
@@ -20,7 +21,17 @@ class ProductProduct(models.Model):
         readonly=True,
     )
 
-    @api.depends_context("product_catalog_order_model", "order_id")
+    def write(self, vals):
+        """
+        Si en vals solo viene product_catalog_qty hacemos esto para que usuarios sin permiso de escribir
+        en productos puedan modificar la cantidad
+        """
+        if len(vals) == 1 and "product_catalog_qty" in vals:
+            qty = vals.get("product_catalog_qty")
+            self._inverse_catalog_values(qty)
+            return True
+        return super().write(vals)
+
     def _compute_catalog_values(self):
         res_model = self._context.get("product_catalog_order_model")
         order_id = self._context.get("order_id")
@@ -32,16 +43,16 @@ class ProductProduct(models.Model):
             rec.product_catalog_qty = order_line_info[rec.id].get("quantity")
             rec.product_catalog_price = order_line_info[rec.id].get("price")
 
-    def _inverse_catalog_values(self):
+    def _inverse_catalog_values(self, product_catalog_qty):
         res_model = self._context.get("product_catalog_order_model")
         order_id = self._context.get("order_id")
         order = self.env[res_model].browse(order_id)
         for rec in self:
-            order.with_company(order.company_id)._update_order_line_info(rec.id, rec.product_catalog_qty)
+            order.with_company(order.company_id)._update_order_line_info(rec.id, product_catalog_qty)
 
     def increase_quantity(self):
-        for rec in self.sudo():
-            rec.product_catalog_qty += 1
+        for rec in self:
+            rec._inverse_catalog_values(rec.product_catalog_qty + 1)
 
     @api.model
     def get_view(self, view_id=None, view_type="form", **options):
@@ -53,15 +64,24 @@ class ProductProduct(models.Model):
 
         catalog_tree_view_id = self.env.ref("product_catalog_tree.product_view_tree_catalog").id
 
-        if view_id and view_id == catalog_tree_view_id and view_type == "tree":
+        if view_id and view_id == catalog_tree_view_id:
             doc = etree.XML(res["arch"])
 
             # make all fields not editable
             for node in doc.xpath("//field"):
                 node.set("readonly", "1")
+                modifiers = json.loads(node.get("modifiers") or "{}")
+                modifiers["readonly"] = True
+                node.set("modifiers", json.dumps(modifiers))
 
+            # make qty field editable
             qty_field = doc.xpath("//field[@name='product_catalog_qty']")[0]
             qty_field.set("readonly", "false")
+
+            for node in doc.xpath("/list"):
+                node.set("edit", "true")
+                node.set("create", "false")
+                node.set("editable", "top")
 
             res["arch"] = etree.tostring(doc, encoding="unicode")
         return res
