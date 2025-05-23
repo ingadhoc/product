c3a652ac8f62787df7b7edd9d1c6cee3b6a787ea
mav-adhoc <mav@adhoc.com.ar>
2025-05-23 16:57:44 -0300

    [IMP]product_currency_mrp: new module which converts prices according to the product currency in bom
    X-original-commit: cce7ab06386026bd8c281cca5e46d4e63f2032ee

---
+++ product_currency_mrp/report/mrp_report_bom_structure.py
@@ -0,0 +1,33 @@
+from odoo import api, fields, models
+
+
+class ReportReplenishmentBomStructure(models.AbstractModel):
+    _inherit = "report.mrp.report_bom_structure"
+
+    @api.model
+    def _get_component_data(
+        self, parent_bom, warehouse, bom_line, line_quantity, level, index, product_info, ignore_stock=False
+    ):
+        res = super()._get_component_data(
+            parent_bom, warehouse, bom_line, line_quantity, level, index, product_info, ignore_stock
+        )
+        currency = self.env.context.get("force_currency") or self.env.company.currency_id
+        company = parent_bom.company_id or self.env.company
+        price = (
+            bom_line.product_id.uom_id._compute_price(
+                bom_line.product_id.with_company(company).standard_price, bom_line.product_uom_id
+            )
+            * line_quantity
+        )
+        price_converted = currency._convert(
+            price, self.env.company.currency_id, (parent_bom.company_id or self.env.company), fields.Date.today()
+        )
+        rounded_price = currency.round(price_converted)
+        res.update(
+            {
+                "currency": currency,
+                "currency_id": currency.id,
+                "prod_cost": rounded_price,
+            }
+        )
+        return res
