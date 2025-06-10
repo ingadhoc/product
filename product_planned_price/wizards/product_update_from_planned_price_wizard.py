9d0d22bd9449e43e921e8708d1bf991a96944099
Juan Ignacio Carreras <jc@adhoc.com.ar>
2025-06-10 20:05:13 +0000

    [FIX]product_planned_price:Force to compute before list_price
    X-original-commit: aec8a72263942da9ac4fd01483d76257f527af1b

--- product_planned_price/wizards/product_update_from_planned_price_wizard.py
+++ product_planned_price/wizards/product_update_from_planned_price_wizard.py
@@ -12,8 +12,30 @@ class ProductUpdateFromPlannedPriceWizard(models.TransientModel):
 
     def confirm(self):
         self.ensure_one()
+<<<<<<< HEAD
         active_ids = self._context.get("active_ids")
         active_model = self._context.get("active_model")
         if active_model != "product.template":
             raise ValidationError(_("Update from planned price must be called from product " "template"))
         return self.env[active_model].browse(active_ids)._update_prices_from_planned()
+||||||| parent of 03be07e0 (temp)
+        active_ids = self._context.get('active_ids')
+        active_model = self._context.get('active_model')
+        if active_model != 'product.template':
+            raise ValidationError(_(
+                'Update from planned price must be called from product '
+                'template'))
+        return self.env[active_model].browse(
+            active_ids)._update_prices_from_planned()
+=======
+        active_ids = self._context.get('active_ids')
+        active_model = self._context.get('active_model')
+        if active_model != 'product.template':
+            raise ValidationError(_(
+                'Update from planned price must be called from product template'))
+        products = self.env[active_model].browse(active_ids)
+        # Hacemos esto para forzar el recomputo antes de llamar la metodo, presumimos que tiene que ver con los depends del _compute_computed_list_price Pero no lo queremos tocar en v16 
+        for prod in products:
+            prod._compute_computed_list_price()
+        products._update_prices_from_planned()
+>>>>>>> 03be07e0 (temp)
