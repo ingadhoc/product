3173167b6c0982da0119a2431f8099d37f15b868
Franco Leyes <lef@adhoc.com.ar>
2025-06-23 13:07:46 -0300

    [IMP] product_catalog_tree: remove search to use original view

--- product_catalog_tree/__manifest__.py
+++ product_catalog_tree/__manifest__.py
@@ -19,7 +19,7 @@
 ##############################################################################
 {
     "name": "Product Catalog Tree",
-    "version": "18.0.1.1.0",
+    "version": "18.0.1.2.0",
     "category": "Products",
     "sequence": 14,
     "summary": "",
