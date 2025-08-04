309da6a2ddcc0cd9200a9568677dc14fea226b94
mav-adhoc <mav@adhoc.com.ar>
2025-08-04 14:20:25 -0300

    [FIX]product_currency_mrp: parent_product in get_bom_data

--- product_currency_mrp/__manifest__.py
+++ product_currency_mrp/__manifest__.py
@@ -19,7 +19,7 @@
 ##############################################################################
 {
     'name': 'Product Currency MRP',
-    'version': "17.0.1.1.0",
+    'version': "17.0.1.2.0",
     'category': 'Products',
     'sequence': 14,
     'summary': '',
