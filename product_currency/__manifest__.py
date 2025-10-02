7d0dba5e8cd5055a01106d2c5db43fdab6fd3224
migration-bot-adhoc <migration-bot-adhoc@users.noreply.github.com>
2025-10-02 19:06:07 +0000

    [MIG] product_currency: Migration to 19.0

--- product_currency/__manifest__.py
+++ product_currency/__manifest__.py
@@ -19,7 +19,7 @@
 ##############################################################################
 {
     "name": "Product Currency",
-    "version": "18.0.1.0.0",
+    "version": "19.0.1.0.0",
     "category": "Products",
     "sequence": 14,
     "summary": "",
@@ -37,7 +37,7 @@
     "demo": [
         "demo/product_product_demo.xml",
     ],
-    "installable": False,
+    "installable": True,
     "auto_install": False,
     "application": False,
 }
