bb23eba14e3d80711640ff6cfc05bc237dd71212
Martin Quinteros <maq@adhoc.com.ar>
2025-10-16 15:13:21 -0300

    [IMP] backport from https://github.com/odoo/odoo/commit/41d8fa352a7baec887210ea7870b475a10f2f39d
    closes ingadhoc/product#789
    
    Signed-off-by: Nicolas Mac Rouillon (ADV) <nmr@adhoc.inc>

--- product_ux/models/product_product.py
+++ product_ux/models/product_product.py
@@ -3,13 +3,26 @@
 # directory
 ##############################################################################
 from odoo import api, fields, models
+from odoo.tools import create_index
 
 
 class ProductProduct(models.Model):
     _inherit = "product.product"
+    _order = "default_code, name, id"
+
+    def init(self):
+        super().init()
+        create_index(
+            self.env.cr,
+            indexname="is_favorite_idx",
+            tablename="product_product",
+            expressions=["is_favorite"],
+            where="is_favorite IS TRUE",
+        )
 
     active = fields.Boolean(tracking=True)
     pricelist_price = fields.Float(compute="_compute_product_pricelist_price", digits="Product Price")
+    is_favorite = fields.Boolean(related="product_tmpl_id.is_favorite", readonly=True, store=True)
 
     @api.depends_context("pricelist", "quantity", "uom", "date", "no_variant_attributes_price_extra")
     def _compute_product_pricelist_price(self):
