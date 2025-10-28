74b7e4ef2580dee5faf5a79929beef3da7564634
migration-bot-adhoc <migration-bot-adhoc@users.noreply.github.com>
2025-10-28 18:56:05 +0000

    [MIG] product_ux: Migration to 19.0

--- product_ux/__manifest__.py
+++ product_ux/__manifest__.py
@@ -19,7 +19,7 @@
 ##############################################################################
 {
     "name": "Product UX",
-    "version": "18.0.1.1.0",
+    "version": "19.0.1.0.0",
     "category": "Products",
     "sequence": 14,
     "summary": "",
@@ -40,7 +40,7 @@
     "demo": [
         "demo/product_pricelist_demo.xml",
     ],
-    "installable": False,
+    "installable": True,
     "auto_install": False,
     "application": False,
 }
