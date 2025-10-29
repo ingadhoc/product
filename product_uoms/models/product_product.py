ba56743b4dfd69bec59faecf5585f583cdca8cdc
migration-bot-adhoc <migration-bot-adhoc@users.noreply.github.com>
2025-10-29 17:54:33 +0000

    [MIG] product_uoms: Migration to 19.0
    Part-of: ingadhoc/product#804
    Signed-off-by: Juan Carreras <jc@adhoc.com.ar>

--- product_uoms/models/product_product.py
+++ product_uoms/models/product_product.py
@@ -15,6 +15,13 @@ class ProductProduct(models.Model):
         We send product uom so it can be send from sale or purchase
         """
         self.ensure_one()
-        return product_uom + self.env["uom.uom"].search(
-            [("category_id", "=", product_uom.category_id.id), ("id", "!=", product_uom.id)]
-        )
+        # Get the root reference UoM by traversing up the parent chain
+        root_uom = product_uom
+        while root_uom.relative_uom_id:
+            root_uom = root_uom.relative_uom_id
+
+        # Find all UoMs that have the same root reference
+        all_uoms = self.env["uom.uom"].search([])
+        compatible_uoms = all_uoms.filtered(lambda u: u._has_common_reference(product_uom) and u.id != product_uom.id)
+
+        return product_uom + compatible_uoms
