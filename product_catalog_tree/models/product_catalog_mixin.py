c61da417ac723fdd3e406bdef843397d38c30e54
Franco Leyes <lef@adhoc.com.ar>
2025-12-24 20:05:31 +0000

    [FIX] product_catalog_tree: add warehouse location filter for sale orders
    X-original-commit: 97a66c3c1815fd341e5595479b0566b55867e77d

--- product_catalog_tree/models/product_catalog_mixin.py
+++ product_catalog_tree/models/product_catalog_mixin.py
@@ -9,4 +9,9 @@ class ProductCatalogMixin(models.AbstractModel):
         action = super().action_add_from_catalog()
         tree_view_id = self.env.ref("product_catalog_tree.product_view_tree_catalog").id
         action["views"] = [(tree_view_id, "list")] + action["views"]
+        # add warehouse location filter if order has warehouse
+        context = action.get("context", {})
+        if "order_id" in context and self._name == "sale.order":
+            if self._fields.get("warehouse_id") and self.warehouse_id:
+                action["context"]["search_default_location_id"] = self.warehouse_id.lot_stock_id.id
         return action
