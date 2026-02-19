5adaf540e1c26dc3ca487f62d163657715f6c6b1
Santiago Co <cos@adhoc.inc>
2026-02-19 11:42:24 -0300

    Desarrollo por Codigo I

---
+++ estate/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    "name": "Real Estate Management",
+    "depends": ["base"],
+    "version": "19.0.0.0.1",
+    "installable": True,
+    "auto_install": False,
+    "application": True,
+    "data": [
+        "security/ir.model.access.csv",
+        "views/estate_property_type_views.xml",
+        "views/estate_property_tag_views.xml",
+        "views/estate_property_offer_views.xml",
+        "views/estate_property_views.xml",
+        "views/estate_menus.xml",
+    ],
+}
