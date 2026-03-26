9c9b03a1d80e3fcda3b4f0b562424a94816c16a4
migration-bot-adhoc <migration-bot-adhoc@users.noreply.github.com>
2026-03-26 15:43:37 +0000

    [MIG] product_catalog_aeroo_report_public_categ: Migration to 19.0

--- product_catalog_aeroo_report_public_categ/__manifest__.py
+++ product_catalog_aeroo_report_public_categ/__manifest__.py
@@ -19,7 +19,7 @@
 ##############################################################################
 {
     "name": "Product Catalog Aeroo Report with public Category",
-    "version": "18.0.1.0.0",
+    "version": "19.0.1.0.0",
     "category": "Aeroo Reporting",
     "sequence": 14,
     "summary": "",
@@ -35,7 +35,7 @@
         "views/product_catalog_report_views.xml",
     ],
     "demo": [],
-    "installable": False,
+    "installable": True,
     "auto_install": True,
     "application": False,
 }
