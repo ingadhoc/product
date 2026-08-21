daf4b84a39d4f495bb72398a100ef045552205b4
roboadhoc <roboadhoc@adhoc.com.ar>
2026-08-21 19:27:51 +0000

    [BOT] Bump version: product_replenishment_cost_mrp 18.0.1.1.0
    Merged: ingadhoc/product#933

--- product_replenishment_cost_mrp/__manifest__.py
+++ product_replenishment_cost_mrp/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Integration between Replenishment Cost and Manufacture",
-    "version": "18.0.1.0.0",
+    "version": "18.0.1.1.0",
     "author": "ADHOC SA, Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "category": "Products",
